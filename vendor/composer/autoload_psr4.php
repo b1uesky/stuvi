00844683d7fd49c6c4ecd2fe2e81c056175d889c
kingdido999 <kingdido999@gmail.com>
2015-04-15 17:06:42 -0400

    use faker to generate users

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -12,6 +12,7 @@ return array(
     'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
     'League\\Flysystem\\' => array($vendorDir . '/league/flysystem/src'),
     'Illuminate\\' => array($vendorDir . '/laravel/framework/src/Illuminate'),
+    'Faker\\' => array($vendorDir . '/fzaninotto/faker/src/Faker'),
     'ClassPreloader\\' => array($vendorDir . '/classpreloader/classpreloader/src'),
     'App\\' => array($baseDir . '/app'),
 );
