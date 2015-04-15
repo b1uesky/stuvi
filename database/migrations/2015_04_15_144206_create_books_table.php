c1e7278516899c5326b275bbbf221049270789d4
kingdido999 <kingdido999@gmail.com>
2015-04-15 10:44:19 -0400

    make model Book

---
+++ database/migrations/2015_04_15_144206_create_books_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('books', function(Blueprint $table)
+		{
+            $table->increments('id');
+            $table->string('title');
+            $table->tinyInteger('edition')->nullable()->default(1);
+            $table->string('author');
+            $table->string('isbn', 13);
+            $table->string('publisher');
+            $table->date('publication_date');
+            $table->string('manufacturer');
+            $table->smallInteger('num_pages');
+            $table->integer('binding_id');
+            $table->integer('image_set_id');
+            $table->integer('language_id');
+            $table->integer('amazon_info_id')->nullable();
+            $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('books');
+	}
+
+}
