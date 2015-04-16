99a1646f54311faca2385ec2cad01f87d3460e1e
B1ueSky <programmer.lty@gmail.com>
2015-04-16 16:52:16 -0400

    change .gitignore and DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
+use App\User;
+use App\Book;
+use App\Product;
+
 class DatabaseSeeder extends Seeder {
 
 	/**
@@ -15,6 +19,29 @@ class DatabaseSeeder extends Seeder {
 		Model::unguard();
 
 		// $this->call('UserTableSeeder');
+
+        // create some test instances.
+        $seller = User::create([    'username'  => 'seller',
+                                    'email'     => 'seller@stuvi.com',
+                                    'password'  => bcrypt('123456')
+        ]);
+        $buyer = User::create([ 'username'  => 'buyer',
+                                'email'     => 'buyer@stuvi.com',
+                                'password'  => bcrypt('123456')
+        ]);
+
+        $book = Book::create([  'title'     => 'Advanced Algorithm',
+                                'author'    => 'Snyder Wayne',
+                                'isbn'      => '1010101010',
+                                'publisher' => 'Boston University',
+                                'manufacturer'  => 'Boston University',
+                                'num_pages' => 1122
+        ]);
+
+        $product = Product::create(['price'     => 99.99,
+                                    'book_id'   => $book->id,
+                                    'seller_id' => $seller->id
+        ]);
 	}
 
 }
