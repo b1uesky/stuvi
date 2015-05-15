c1a820fc5167c04bba5fb36c7fc9149a048902bd
patels95 <patels326@gmail.com>
2015-05-14 20:45:15 -0400

    added login.js

---
+++ public/js/login.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Sanam on 5/14/15.
+ */
+
+$(document).ready(function(){
+    $("#tabs").tab();
+});
+
+
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,9 @@
 @extends('app')
 <head>
     <link href="{{ asset('/css/login.css') }}" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('/js/login.js')}}" type="text/javascript"></script>
 </head>
 @section('content')
     <div class="container-fluid">
@@ -8,9 +11,9 @@
         <div class="row vertical-center">
             <div class="col-sm-6 col-sm-offset-3">
                 <div class="container" id="form-container">
-                    <ul class="nav nav-tabs nav-justified" role="tablist">
-                        <li role="presentation" class="active"><a href="#" aria-controls="home" role="tab" data-toggle="tab">Log In</a></li>
-                        <li role="presentation"><a href="#" aria-controls="profile" role="tab" data-toggle="tab">Sign Up</a></li>
+                    <ul class="nav nav-tabs nav-justified" role="tablist" id="tabs">
+                        <li role="presentation" class="active" id="login-tab"><a href="#login-body" aria-controls="login-body" role="tab" data-toggle="tab">Log In</a></li>
+                        <li role="presentation" id="signup-tab"><a href="#signup-body" aria-controls="signup-body" role="tab" data-toggle="tab">Sign Up</a></li>
                     </ul>
                     {{--<div class="panel-heading" id="login-heading">--}}
                         {{--<ul class="nav nav-tabs">--}}
@@ -29,28 +32,21 @@
                         </div>
                     @endif
                     <div class="tab-content">
-                        <div class="tab-pane" id="login-body">
+                        <div class="tab-pane active" id="login-body">
                             <form class="form-horizontal login-form" role="form" method="POST" action="{{ url('/auth/login') }}">
                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
                                 <div class="form-group">
-                                    <label class="col-md-4 control-label">E-Mail Address</label>
-
-                                    <div class="col-md-6">
-                                        <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                                    <div class="col-md-12">
+                                        <input type="email" class="form-control" name="email" placeholder="Email" value="{{ old('email') }}">
                                     </div>
                                 </div>
-
                                 <div class="form-group">
-                                    <label class="col-md-4 control-label">Password</label>
-
-                                    <div class="col-md-6">
-                                        <input type="password" class="form-control" name="password">
+                                    <div class="col-md-12">
+                                        <input type="password" class="form-control" name="password" placeholder="Password">
                                     </div>
                                 </div>
-
                                 <div class="form-group">
-                                    <div class="col-md-6 col-md-offset-4">
+                                    <div class="col-md-6">
                                         <div class="checkbox">
                                             <label>
                                                 <input type="checkbox" name="remember"> Remember Me
@@ -58,63 +54,50 @@
                                         </div>
                                     </div>
                                 </div>
-
                                 <div class="form-group">
-                                    <div class="col-md-6 col-md-offset-4">
+                                    <div class="col-md-12">
                                         <button type="submit" class="btn btn-primary login-button">Login</button>
-
-                                        <a class="btn btn-link" id="forgot-password" href="{{ url('/password/email') }}">Forgot
-                                            Your Password?</a>
+                                        <a class="btn btn-link" id="forgot-password" href="{{ url('/password/email') }}">
+                                            Forgot Your Password?
+                                        </a>
                                     </div>
                                 </div>
                             </form>
                         </div>
-                        <div class="tab-pane active" id="signup-body">
+                        <div class="tab-pane" id="signup-body">
                             <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
-                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">\
 
                                 <div class="form-group">
-                                    <label class="col-lg-4 control-label">First Name</label>
-                                    <div class="col-lg-6">
-                                        <input type="text" class="form-control" name="first_name" value="{{ old('first_name') }}">
+                                    <div class="col-md-12">
+                                        <input type="text" class="form-control" name="first_name" placeholder="First Name" value="{{ old('first_name') }}">
                                     </div>
                                 </div>
-
                                 <div class="form-group">
-                                    <label class="col-lg-4 control-label">Last Name</label>
-                                    <div class="col-lg-6">
-                                        <input type="text" class="form-control" name="last_name" value="{{ old('last_name') }}">
+                                    <div class="col-md-12">
+                                        <input type="text" class="form-control" name="last_name" placeholder="Last Name" value="{{ old('last_name') }}">
                                     </div>
                                 </div>
-
                                 <div class="form-group">
-                                    <label class="col-lg-4 control-label">E-Mail Address</label>
-                                    <div class="col-lg-6">
-                                        <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                                    <div class="col-md-12">
+                                        <input type="email" class="form-control" name="email" placeholder="Email" value="{{ old('email') }}">
                                     </div>
                                 </div>
-
                                 <div class="form-group">
-                                    <label class="col-lg-4 control-label">Password</label>
-                                    <div class="col-lg-6">
-                                        <input type="password" class="form-control" name="password">
+                                    <div class="col-md-12">
+                                        <input type="password" class="form-control" name="password" placeholder="Password">
                                     </div>
                                 </div>
-
                                 <div class="form-group">
-                                    <label class="col-lg-4 control-label">Phone Number</label>
-                                    <div class="col-lg-1">
-                                        <input type="number" class="form-control" name="phone_number" value="{{ old('phone_number') }}">
+                                    <div class="col-md-12">
+                                        <input type="number" class="form-control" name="phone_number" placeholder="Phone Number" value="{{ old('phone_number') }}">
                                     </div>
-                                    {{--<span>(Optional)</span>--}}
                                 </div>
-
                                 <div class="form-group">
-                                    <div class="col-lg-6 col-lg-offset-4">
+                                    <div class="col-md-6">
                                         <button type="submit" class="btn btn-primary login-button">Register</button>
                                     </div>
                                 </div>
-
                                 {{--<div class="form-group">--}}
                                 {{--<label class="col-md-4 control-label">Username</label>--}}
                                 {{--<div class="col-md-6">--}}
