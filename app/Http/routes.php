76fa16741ef220aa46fed0251773495356007f33
nlouie <louie.nick@yahoo.com>
2015-05-25 15:18:53 -0400

    commented out from home controller (this middleware) removed code from home.blade.php to start adding to it

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'HomeController@login');
+Route::get('/', 'HomeController@index');
 Route::get('/home', 'HomeController@login');
 Route::get('/login', 'HomeController@login');
 Route::get('/register', 'HomeController@register');
