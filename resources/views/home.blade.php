c63d37226f01f350cfd8b1937dcc27f269dec967
patels95 <patels326@gmail.com>
2015-05-27 16:05:13 -0400

    updated navigation dropdown

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -79,8 +79,6 @@
 
 
     <!-- JAVASCRIPT at end for faster loading -->
-    <script src="{{url('cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js')}}"></script>
-    <script src="{{asset('js/bootstrap.min.js')}}" type="text/javascript"></script>
     <script src="{{asset('/js/login.js')}}" type="text/javascript"></script>
 
 
