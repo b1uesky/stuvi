5ac9f16cc699923cdb52405fb0f73070deba9302
kingdido999 <kingdido999@gmail.com>
2015-06-01 15:58:41 -0400

    add create method for TextbookController

--- app/Http/Controllers/TextbookController.php
+++ app/Http/Controllers/TextbookController.php
@@ -25,7 +25,15 @@ class TextbookController extends Controller {
 		return view('textbook.buy');
 	}
 
-
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        return view('textbook.create');
+    }
 
 	/**
 	 * Store a newly created resource in storage.
