fce256f4ba523b2dd06949693fdde7ef140ac540
nlouie <louie.nick@yahoo.com>
2015-06-02 11:30:24 -0400

    User-bar

--- resources/views/user/account.blade.php
+++ resources/views/user/account.blade.php
@@ -10,9 +10,30 @@
     <!-- top bar -->
     <div class="container-fluid" id = "user-bar">
         <ul class="nav nav-tabs nav-justified" role="tablist">
-            <li><a href="{{url('/user/profile')}}">Profile</a></li>
-            <li class = "active"><a href="{{url('/user/account')}}">Account</a></li>
-            <li><a href="#">Orders</a></li>
+            <li class="active"><a href="{{url('/user/profile')}}">Profile</a></li>
+            <!-- Account dropdown -->
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle tabz" data-toggle="dropdown"> Account<span class="caret"></span> </a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="{{url('/user/account')}}" class="acc">Account Settings</a></li>
+                    <li><a href="#"                        class="acc">Your Courses</a></li>
+                    <li><a href="#"                        class="acc">Messages</a></li>
+                </ul>
+            </li>
+            <!-- Textbooks dropdown -->
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle tabz" data-toggle="dropdown">Books <span class="caret"></span> </a>
+                <ul class="dropdown-menu" role="menu">
+                    <li role="presentation" class="dropdown-header">Buy</li>
+                    <li><a href="#" data-toggle="tab" class="">Your Orders</a></li>
+                    <li><a href="#" data-toggle="tab" class="">Your Wish List</a></li>
+                    <li><a href="#" data-toggle="tab" class="">Returns</a></li>
+                    <li role="presentation" class="divider"></li>
+                    <li role="presentation" class="dropdown-header">Sell</li>
+                    <li><a href="#" data-toggle="tab">All Selling</a></li>
+                    <li><a href="#" data-toggle="tab">Sold</a></li>
+                </ul>
+            </li>
             <li class = "disabled"><a href="#">Clubs</a></li>
             <li class = "disabled"><a href="#">Groups</a></li>
         </ul>
