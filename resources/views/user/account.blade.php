eb260530da276d8415755ae5c3fd5d7f20ad9df7
nlouie <louie.nick@yahoo.com>
2015-06-03 12:25:33 -0400

    Account and profile page

--- resources/views/user/account.blade.php
+++ resources/views/user/account.blade.php
@@ -1,11 +1,12 @@
 @extends('app')
+
+
+@section('content')
     <head>
         <title>Stuvi - Account</title>
         <link href="{{ asset('/css/account.css') }}" type="text/css" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="{{asset('/css/user-bar.css')}}"
     </head>
-
-@section('content')
 
     <!-- top bar -->
     <div class="container-fluid" id = "user-bar">
@@ -43,17 +44,70 @@
     <!-- My account container -->
     <div class = "container-fluid bg" id = "pad">
 
-        <div class="container">
-            <div class = "container pad"><h1>My Account</h1></div>
+        <div class="container col-md-10">
+            <div class = "container"><h1>My Account</h1></div>
+            <!-- account container -->
             <div class = "container span6 account">
-                <h3>Quick Links</h3>
-                <hr id="hr1">
-                <ul>
-                    <li><a href="#">Account Settings</a></li>
-                    <li><a href="#">Messages</a></li>
-                    <li><a href="#">My Courses</a></li>
-                    <li><a href="#">Forgot your password?</a></li>
-                </ul>
+                <!-- quick links -->
+                <div class="container quick-links">
+                    <h3>Quick Links</h3>
+                    <hr>
+                    <ul>
+                        <li><a href="#">Messages</a></li>
+                        <li><a href="#">Your Courses</a></li>
+                        <li><a href="{{url('/password/email')}}">Forgot your password?</a></li>
+                    </ul>
+                </div>
+                <!-- change account information -->
+                <div class="container account-edit">
+                    <h3>Edit your information</h3>
+                    <hr>
+                    <form class="form-horizontal" role="form">
+                        <!-- First name -->
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="first-name">First name:</label>
+                            <div class="col-sm-6">
+                                <input type="text" class="form-control" name="first-name" id="fname" placeholder="First name">
+                            </div>
+                        </div>
+
+                        <!-- Last name -->
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="first-name">Last name:</label>
+                            <div class="col-sm-6">
+                                <input type="text" class="form-control" name="last-name" id="lname" placeholder="Last name">
+                            </div>
+                        </div>
+
+                        <!-- Email -->
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="email">Email:</label>
+                            <div class="col-sm-6">
+                                <input type="email" class="form-control" id="email" placeholder="Enter email">
+                            </div>
+                        </div>
+                        <!-- Change password...current password -->
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="pwd">Current Password:</label>
+                            <div class="col-sm-6">
+                                <input type="password" class="form-control" id="pwd" placeholder="Enter current password">
+                            </div>
+                        </div>
+                        <!-- New password -->
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="new-pwd">New Password:</label>
+                            <div class="col-sm-6">
+                                <input type="password" class="form-control" id="new-pwd" placeholder="Enter new password">
+                            </div>
+                        </div>
+                        <!-- Save changes button -->
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-6">
+                                <button type="submit" class="btn btn-default">Save Changes</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
 
         </div>
