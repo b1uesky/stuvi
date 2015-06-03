4e3a2385b7849a48a42bbdd5fd900c572bfce109
kingdido999 <kingdido999@gmail.com>
2015-06-03 11:47:07 -0400

    add verified field to products table

---
+++ database/migrations/2015_06_03_154228_add_verified_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddVerifiedProductsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('products', function(Blueprint $table)
+		{
+			$table->boolean('verified')->default(false);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('products', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
