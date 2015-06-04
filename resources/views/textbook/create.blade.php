07a27c552ae5e0ec9b189f73b1bbbb43b22661e1
nlouie <louie.nick@yahoo.com>
2015-06-04 16:28:27 -0400

    Create textbook styling

--- resources/views/textbook/create.blade.php
+++ resources/views/textbook/create.blade.php
@@ -10,7 +10,7 @@
         {{--<li class="active"><a href="{{ url('/textbook/sell') }}">Sell</a></li>--}}
     {{--</ul>--}}
 
-<div class = "container-fluid">
+<div class = "container-fluid create">
     <!-- add padding -->
     <div class="container col-md-8 col-md-offset-2 pad">
         <div class="row">
@@ -23,7 +23,7 @@
             <!-- TODO: These fields are not specific enough. Please see http://easybib.com/mla-format/book-citation for an example -->
             <h1>Enter your textbook information</h1>
 
-            <form action="/textbook/sell/store" method="post" id="textbook-create" enctype="multipart/form-data">
+            <form action="/textbook/sell/store" method="post" id="textbook-create" class="form" enctype="multipart/form-data">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                 <div class="form-group">
                     <label><b>ISBN</b></label>
@@ -57,7 +57,7 @@
                     <label><b>Number of Pages</b></label>
                     <input type="number" name="num_pages" value="{{ $book->num_pages or 0 }}" class="form-control"/>
                 </div>
-                <div class="form-group">
+                <div class="form-group ws" id="binding">
                     <label><b>Binding</b></label>
                     <input type="radio" name="binding" value="1" checked/> Hard
                     <input type="radio" name="binding" value="2"/> Soft
@@ -66,15 +66,15 @@
                 <!-- TODO: a complete list of languages and their ids -->
                 <div class="form-group">
                     <label><b>Language</b></label>
-                    <select name="language" id="textbook-create">
-                        <option value="1">English</option>
-                        <option value="2">Spanish</option>
-                        <option value="3">Chinese</option>
+                    <select name="language" id="textbook-create lang">
+                        <option value="1" class="lang-select">English</option>
+                        <option value="2" class="lang-select">Spanish</option>
+                        <option value="3" class="lang-select">Chinese</option>
                     </select>
                 </div>
 
-                <div class="form-group">
-                    <label>Cover Image</label>
+                <div class="form-group ws">
+                    <label >Cover Image</label>
                     <input type="file" name="image"/>
                 </div>
                 <input type="submit" name="submit" class="btn btn-primary" value="Submit"/>
