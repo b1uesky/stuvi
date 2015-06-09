2246526fe5ef80dcca2d68ceb92f8b9a44f33c13
kingdido999 <kingdido999@gmail.com>
2015-06-09 11:58:47 -0400

    update Book model

--- app/Book.php
+++ app/Book.php
@@ -6,27 +6,27 @@ class Book extends Model {
 
 	public function binding()
     {
-        return $this->hasOne('BookBinding');
+        return $this->hasOne('App\BookBinding');
     }
 
     public function imageSet()
     {
-        return $this->hasOne('BookImageSet');
+        return $this->hasOne('App\BookImageSet');
     }
 
     public function language()
     {
-        return $this->hasOne('BookLanguage');
+        return $this->hasOne('App\BookLanguage');
     }
 
     public function amazonInfo()
     {
-        return $this->hasOne('BookAmazonInfo');
+        return $this->hasOne('App\BookAmazonInfo');
     }
 
     public function products()
     {
-        return $this->hasMany('Product');
+        return $this->hasMany('App\Product');
     }
 
 }
