091353a25e32c8f9ee9218d70e091673c24aefb6
nlouie <louie.nick@yahoo.com>
2015-06-16 15:19:49 -0400

    order

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -38,8 +38,17 @@
                         </div>
                     </div>
                     <!-- order status -->
-                    <h3>Order Processing</h3>
-                    <small>Your order is being processed by the Stuvi team.</small>
+                    @if ($order->cancelled)
+                        <span id="cancelled"> <h3>Order Canceled</h3>
+                        <small>Your order has been cancelled.</small>
+                        </span>
+                    @elseif ($order->pickup_time)
+                        <h3>Delivered</h3>
+                        <small>Delivered at {{ date($datetime_format, strtotime($order->pickup_time)) }}</small>
+                    @else
+                        <h3>Order Processing</h3>
+                        <small>Your order is being processed by the Stuvi team.</small>
+                    @endif
                     @foreach($order->products() as $product)
                         <div class="row book-row">
                             <div class="col-xs-12 col-sm-2 book-img">
