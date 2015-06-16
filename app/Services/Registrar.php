80bc0dcad09c2447c4a0e6f873d68183a92d86f2
kingdido999 <kingdido999@gmail.com>
2015-06-16 17:03:21 -0400

    add welcome email message after registration

--- app/Services/Registrar.php
+++ app/Services/Registrar.php
@@ -2,6 +2,7 @@
 
 use App\User;
 use Validator;
+use Mail;
 use Illuminate\Contracts\Auth\Registrar as RegistrarContract;
 
 class Registrar implements RegistrarContract {
@@ -28,13 +29,20 @@ class Registrar implements RegistrarContract {
 	 */
 	public function create(array $data)
 	{
-		return User::create([
+		$user = User::create([
 			'email' => $data['email'],
 			'password' => bcrypt($data['password']),
             'phone_number' => $data['phone_number'],
             'first_name' => $data['first_name'],
             'last_name' => $data['last_name'],
 		]);
+
+		Mail::send('emails.welcome', ['first_name' => $data['first_name']], function($message)
+		{
+		    $message->to('kingdido999@gmail.com', $data['first_name'].$data['last_name'])->subject('Welcome!');
+		});
+
+		return $user;
 	}
 
 }
