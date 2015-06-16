28e3ceb3de95257375425fa12ea3d0db43ff88d2
kingdido999 <kingdido999@gmail.com>
2015-06-16 14:40:06 -0400

    move amazon credentials into services

--- config/amazon.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Amazon Product Advertising API credentials
-
-return [
-    'access_key_id'     =>  'AKIAICSPEAJYPA3CBSNQ',
-    'secret_access_key' =>  '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',
-    'associate_id'      =>  'stuvi07-20'
-];
