80232ce3e0e485dd614918ade0590a9ddea9de2c
patels95 <patels326@gmail.com>
2015-06-18 16:21:51 -0400

    updated product-create. added bootstrap radio buttons

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -1,38 +1,50 @@
 @extends('textbook')
 
 @section('content')
+    <head>
+        <link href="{{ asset('/css/product-create.css') }}" rel="stylesheet">
+        <title>Enter book info</title>
+    </head>
+
     <div class="container">
-        <div class="row">
-            @if($book->imageSet->large_image)
-                <img src="{{ $book->imageSet->large_image }}" alt=""/>
-            @endif
-
-            <div class="">
-                Title: {{ $book->title }}
-            </div>
-
-            <div class="">
-                Edition: {{ $book->edition }}
-            </div>
-
-            <div class="">
-                ISBN: {{ $book->isbn }}
-            </div>
-
-            {{-- Author(s) --}}
-            {{-- TODO: Make each author name looks like a tag --}}
-            <div class="">
-                @if(count($book->authors) > 1)
-                    <span>Authors:</span>
-                    @foreach($book->authors as $author)
-                        <span>{{ $author->full_name }}</span>
-                    @endforeach
-                @else
-                    <span>Author:</span>
-                    {{ $book->authors[0]->full_name }}
+        <div class="row textbook-row">
+            <div class="col-sm-6">
+                @if($book->imageSet->large_image)
+                    <img id="textbook-img" src="{{ $book->imageSet->large_image }}" alt=""/>
                 @endif
             </div>
 
+            <div class="col-sm-6 textbook-info">
+                <h1>{{ $book->title }}</h1>
+
+                <div class="authors-container">
+                    <span>by </span>
+                    @foreach($book->authors as $author)
+                        <span id="authors"><button class="btn btn-default author-btn">{{ $author->full_name }}</button></span>
+                    @endforeach
+                </div>
+                <p>ISBN: {{ $book->isbn }}</p>
+
+                <p>Edition: {{ $book->edition }}</p>
+
+                <p>Number of Pages: {{ $book->num_pages }}</p>
+                {{-- Author(s) --}}
+                {{-- TODO: Make each author name looks like a tag --}}
+                {{--<div class="">--}}
+                {{--@if(count($book->authors) > 1)--}}
+                {{--<span>Authors:</span>--}}
+                {{--@foreach($book->authors as $author)--}}
+                {{--<span>{{ $author->full_name }}</span>--}}
+                {{--@endforeach--}}
+                {{--@else--}}
+                {{--<span>Author:</span>--}}
+                {{--{{ $book->authors[0]->full_name }}--}}
+                {{--@endif--}}
+                {{--</div>--}}
+
+            </div>
+        </div>
+        <div class="row">
             <h2>Book Conditions</h2>
 
             <form action="/textbook/sell/product/store" method="post" enctype="multipart/form-data">
@@ -43,16 +55,32 @@
                 {{-- General Condition --}}
                 <div class="form-group">
                     <label>{{ $conditions['general_condition']['title'] }}</label>
-                    <input type="radio" name="general_condition" value="0"
-                           checked> {{ $conditions['general_condition'][0] }}
-                    <input type="radio" name="general_condition" value="1"> {{ $conditions['general_condition'][1] }}
-                    <input type="radio" name="general_condition" value="2"> {{ $conditions['general_condition'][2] }}
-                    <input type="radio" name="general_condition" value="3"> {{ $conditions['general_condition'][3] }}
+                    <br>
+
+                    <div class="btn-group" data-toggle="buttons">
+                        <label class="btn btn-default condition-btn">
+                            <input type="radio" name="general_condition" value="0"
+                                   checked> {{ $conditions['general_condition'][0] }}
+                        </label>
+                        <label class="btn btn-default condition-btn">
+                            <input type="radio" name="general_condition"
+                                   value="1"> {{ $conditions['general_condition'][1] }}
+                        </label>
+                        <label class="btn btn-default condition-btn">
+                            <input type="radio" name="general_condition"
+                                   value="2"> {{ $conditions['general_condition'][2] }}
+                        </label>
+                        <label class="btn btn-default condition-btn">
+                            <input type="radio" name="general_condition"
+                                   value="3"> {{ $conditions['general_condition'][3] }}
+                        </label>
+                    </div>
                 </div>
 
                 {{-- Highlights/Notes --}}
                 <div class="form-group">
                     <label>{{ $conditions['highlights_and_notes']['title'] }}</label>
+                    <br>
                     <input type="radio" name="highlights_and_notes" value="0"
                            checked> {{ $conditions['highlights_and_notes'][0] }}
                     <input type="radio" name="highlights_and_notes"
@@ -64,6 +92,7 @@
                 {{-- Damaged Pages --}}
                 <div class="form-group">
                     <label>{{ $conditions['damaged_pages']['title'] }}</label>
+                    <br>
                     <input type="radio" name="damaged_pages" value="0" checked> {{ $conditions['damaged_pages'][0] }}
                     <input type="radio" name="damaged_pages" value="1"> {{ $conditions['damaged_pages'][1] }}
                     <input type="radio" name="damaged_pages" value="2"> {{ $conditions['damaged_pages'][2] }}
@@ -72,6 +101,7 @@
                 {{-- Broken Binding --}}
                 <div class="form-group">
                     <label>{{ $conditions['broken_binding']['title'] }}</label>
+                    <br>
                     <input type="radio" name="broken_binding" value="0" checked> {{ $conditions['broken_binding'][0] }}
                     <input type="radio" name="broken_binding" value="1"> {{ $conditions['broken_binding'][1] }}
                 </div>
