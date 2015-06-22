24a379803e7d52adab7919f4616c4ee094337811
kingdido999 <kingdido999@gmail.com>
2015-06-22 12:27:37 -0400

    layout change

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Product;
+use Config;
 
 class ProductController extends Controller {
 
@@ -49,7 +50,9 @@ class ProductController extends Controller {
 	 */
 	public function show($id)
 	{
-        return view('admin.product.show')->withProduct($id);
+        return view('admin.product.show')
+            ->withProduct($id)
+            ->withConditions(Config::get('product.conditions'));
 	}
 
 	/**
--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -1,58 +1,55 @@
 @extends('admin')
 
 @section('content')
-    <div class="container">
 
-        <nav class="navbar navbar-inverse">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="{{ URL::to('admin/product') }}">Product</a>
-            </div>
-            <ul class="nav navbar-nav">
-                <li><a href="{{ URL::to('admin/product') }}">View All Products</a></li>
-                {{--<li><a href="{{ URL::to('product/create') }}">Create a Product</a>--}}
-            </ul>
-        </nav>
-
-        <div class="row">
-            <table class="table table-hover">
-                <tr>
-                    <th>ID</th>
-                    <th>Book Title</th>
-                    <th>Price</th>
-                    <th>Seller Email</th>
-                    <th>Images</th>
-                    <th>Sold</th>
-                    <th>Verified</th>
-                    <th>Detail</th>
-                </tr>
-
-                @foreach($products as $product)
-                    <tr>
-                        <td>{{ $product->id }}</td>
-                        <td>{{ $product->book->title }}</td>
-                        <td>{{ $product->price }}</td>
-                        <td>{{ $product->seller->email }}</td>
-                        <td>
-                            @foreach($product->images as $image)
-                                <a href="{{ $image->path }}" target="_blank"><img src="{{ $image->path }}" alt=""/></a>
-                            @endforeach
-                        </td>
-                        <td>{{ $product->isSold() }}</td>
-                        <td>{{ $product->verified }}</td>
-
-                        <!-- we will also add show, edit, and delete buttons -->
-                        <td>
-
-                            <!-- delete the nerd (uses the destroy method DESTROY /nerds/{id} -->
-                            <!-- we will add this later since its a little more complicated than the other two buttons -->
-
-                            <!-- show the nerd (uses the show method found at GET /nerds/{id} -->
-                            <a class="btn btn-small btn-success" href="{{ URL::to('admin/product/' . $product->id) }}">Details</a>
-                        </td>
-                    </tr>
-                @endforeach
-
-            </table>
+    <nav class="navbar navbar-default">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="{{ URL::to('admin/product') }}">Product</a>
         </div>
-    </div>
+        <ul class="nav navbar-nav">
+            <li><a href="{{ URL::to('admin/product') }}">View All Products</a></li>
+            {{--<li><a href="{{ URL::to('product/create') }}">Create a Product</a>--}}
+        </ul>
+    </nav>
+
+        <table class="table table-hover">
+            <tr>
+                <th>ID</th>
+                <th>Book Title</th>
+                <th>Price</th>
+                <th>Seller Email</th>
+                <th>Images</th>
+                <th>Sold</th>
+                <th>Verified</th>
+                <th>Actions</th>
+            </tr>
+
+            @foreach($products as $product)
+                <tr>
+                    <td>{{ $product->id }}</td>
+                    <td>{{ $product->book->title }}</td>
+                    <td>{{ $product->price }}</td>
+                    <td>{{ $product->seller->email }}</td>
+                    <td>
+                        @foreach($product->images as $image)
+                            <a href="{{ $image->path }}" target="_blank"><img src="{{ $image->path }}" alt=""/></a>
+                        @endforeach
+                    </td>
+                    <td>{{ $product->isSold() }}</td>
+                    <td>{{ $product->verified }}</td>
+
+                    <!-- we will also add show, edit, and delete buttons -->
+                    <td>
+
+                        <!-- delete the nerd (uses the destroy method DESTROY /nerds/{id} -->
+                        <!-- we will add this later since its a little more complicated than the other two buttons -->
+
+                        <!-- show the nerd (uses the show method found at GET /nerds/{id} -->
+                        <a class="btn btn-small btn-success" href="{{ URL::to('admin/product/' . $product->id) }}">Detail</a>
+                    </td>
+                </tr>
+            @endforeach
+
+        </table>
+
 @endsection
