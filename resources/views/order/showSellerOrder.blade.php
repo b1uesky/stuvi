5dd8ac8c9453852ee95a82db639607e7223c0b40
nlouie <louie.nick@yahoo.com>
2015-06-24 14:51:40 -0400

    clean up. + sorry no results found

--- resources/views/order/showSellerOrder.blade.php
+++ resources/views/order/showSellerOrder.blade.php
@@ -50,9 +50,6 @@
             <!-- item info -->
             <div class="item col-xs-12 col-sm-6">
                 <?php $product = $seller_order->product; $book = $product->book; ?>
-{{--                <p><label class="col-md-4 control-label">Title: {{ $book->title }}</label></p>
-                <p><label class="col-md-4 control-label">ISBN: {{ $book->isbn }}</label></p>
-                <p><label class="col-md-4 control-label">Price: {{ $product->price }}</label></p>--}}
                     <p>Title: <a href="{{ url('/textbook/buy/product/'.$product->id) }}">{{ $book->title }}</a></p>
                     <p>ISBN: {{ $book->isbn10 }}</p>
                     <p>Price: ${{ $product->price }}</p>
