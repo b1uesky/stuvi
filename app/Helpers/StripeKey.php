1c0e27c93e103118477d15f392eae9ea986c9ed2
B1ueSky <programmer.lty@gmail.com>
2015-06-25 14:17:17 -0400

    Add helper functions in functions.php and get Stripe secret and public key according to the app environment.

---
+++ app/Helpers/StripeKey.php
@@ -0,0 +1,30 @@
+<?php namespace App\Helpers;
+/**
+ * Created by PhpStorm.
+ * User: Tianyou Luo
+ * Date: 6/25/15
+ * Time: 2:08 PM
+ */
+
+class StripeKey
+{
+    /**
+     * Get Stripe secret key according to the current app environment.
+     *
+     * @return mixed
+     */
+    public static function getStripeSecretKey()
+    {
+        return isProductionEnv() ? Config::get('stripe.live_secret_key') : Config::get('stripe.test_secret_key');
+    }
+
+    /**
+     * Get Stripe public key according to the current app environment.
+     *
+     * @return mixed
+     */
+    public static function getStripePublicKey()
+    {
+        return isProductionEnv() ? Config::get('stripe.live_public_key') : Config::get('stripe.test_public_key');
+    }
+}
