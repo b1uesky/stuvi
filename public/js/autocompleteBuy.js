8656cc53251554a01bbf3d7f2679f1cc754ac0af
kingdido999 <kingdido999@gmail.com>
2015-06-29 17:02:11 -0400

    add authors, isbn and image to autocomplete results

--- public/js/autocompleteBuy.js
+++ public/js/autocompleteBuy.js
@@ -12,5 +12,20 @@ $(document).ready(function() {
             // go to the book's url
             window.location.href = "/textbook/buy/textbook/" + ui.item.id;
         }
-    });
+    }).autocomplete('instance')._renderItem = function(ul, item) {
+        return $("<li>")
+            .append(
+                '<div class="autocomplete-result">' +
+                    '<div class="autocomplete-thumbnail">' +
+                        '<img src="' + item.image + '">' +
+                    '</div>' +
+                    '<div class="autocomplete-data">' +
+                        '<div class="autocomplete-title">' + item.title + '</div>' +
+                        '<div class="autocomplete-authors">' + item.authors.join(', ') + '</div>' +
+                        '<div class="autocomplete-isbn">ISBN-10:' + item.isbn10 + '</div>' +
+                        '<div class="autocomplete-isbn">ISBN-13:' + item.isbn13 + '</div>' +
+                    '</div>' +
+                '</div>')
+            .appendTo(ul);
+    };;
 });
