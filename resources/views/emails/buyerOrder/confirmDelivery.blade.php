5ff9bf27b3a1b972537537c1bdf473e71611f305
nlouie <louie.nick@yahoo.com>
2015-07-01 14:00:58 -0400

    order ready email

--- resources/views/emails/buyerOrder/confirmDelivery.blade.php
+++ resources/views/emails/buyerOrder/confirmDelivery.blade.php
@@ -7,7 +7,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html" charset=UTF-8" />
-    <title>Welcome to Stuvi</title>
+    <title>Your order has been delivered</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 
