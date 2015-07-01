a9cec68d353fffd54e71a2d2ece1d0c4aa4fc045
B1ueSky <programmer.lty@gmail.com>
2015-07-01 11:47:44 -0400

    Working on activation workflow.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -21,6 +21,8 @@ class AuthController extends Controller {
 
 	use AuthenticatesAndRegistersUsers;
 
+    protected $redirectPath = '/user/activate';
+
 	/**
 	 * Create a new authentication controller instance.
 	 *
---
+++ resources/views/user/waitForActivation.blade.php
@@ -0,0 +1,21 @@
+{{--User Account Settings page--}}
+
+@extends('app')
+
+@section('content')
+    <head>
+        <title> Stuvi - Wait for Activation </title>
+        <link href="{{ asset('/css/user/account.css') }}" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="{{asset('/css/user-bar.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('/css/user-profile.css')}}">
+
+    </head>
+
+    <p>Please check your email box and activate your account via the link sent with our welcome email. Thank you!</p>
+
+    <!-- required for active class in nav to work -->
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="{{asset('/js/account.js')}}" type="text/javascript"></script>
+
+
+@endsection
