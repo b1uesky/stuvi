cb192fe045393e7dffa618b4afff6cefe4c93cb8
kingdido999 <kingdido999@gmail.com>
2015-07-03 10:49:15 -0400

    refactor showSellerOrder.js

--- public/js/order/seller/showSellerOrder.js
+++ public/js/order/seller/showSellerOrder.js
@@ -2,16 +2,16 @@
  * Created by nlouie on 6/16/15.
  */
 
-/*
-<!-- New date time picker
-http://xdsoft.net/jqplugins/datetimepicker/ */
-
-jQuery('#datetimepicker').datetimepicker({
-    format: 'm/d/Y H:i',
-    minDate:'-1970/01/01',//yesterday is minimum date(for today use 0 or -1970/01/01)
-    //maxDate:'+1970/01/02'//tommorow is maximum date calendar
-    minTime: 0,
-    //mask:true, // '9999/19/39 29:59' - digit is the maximum possible for a cell
-    lang: 'en',
-    step: 30
+$(document).ready(function() {
+    // Date time picker
+    // http://xdsoft.net/jqplugins/datetimepicker/
+   $('#datetimepicker').datetimepicker({
+       format: 'm/d/Y H:i',
+       minDate:'-1970/01/01',//yesterday is minimum date(for today use 0 or -1970/01/01)
+       //maxDate:'+1970/01/02'//tommorow is maximum date calendar
+       minTime: 0,
+       //mask:true, // '9999/19/39 29:59' - digit is the maximum possible for a cell
+       lang: 'en',
+       step: 30
+   });
 });
