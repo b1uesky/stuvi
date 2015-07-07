b0c451016d5a10c92c616e28b3feb31c8784b4e1
taoyiran <taoyiran@bu.edu>
2015-07-07 15:55:33 -0400

    cart-items

--- app/Cart.php
+++ app/Cart.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\CartItem;
 
 class Cart extends Model
 {
@@ -15,6 +16,35 @@ class Cart extends Model
     {
         return $this->belongsTo('App\User', 'user_id', 'id');
     }
+
+    /**
+     * Delete items in cart by items' id.
+     *
+     * @param $cart_items_id
+     */
+
+    public function remove_items($,$cart_items_id) {
+        $cart_item =
+
+    }
+
+    public function isValid(){
+        $cart_items =  CartItem::where('cart_id','=', )->get();
+        foreach ($cart_items as $items) {
+
+
+        }
+
+
+    }
+
+    public function validate(){
+
+
+
+    }
+
+
 
 
 
