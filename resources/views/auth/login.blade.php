da82578a95760403627347f8d1c7218eb946df96
patels95 <patels326@gmail.com>
2015-07-07 15:53:55 -0400

    added terms and privacy modals to register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -143,8 +143,10 @@
                                     </div>
                                 </div>
                                 <div class="tos">
-                                    By creating an account, you agree to Stuvi's <a href="#">Term of Use</a> and
-                                    <a href="#">Privacy Notice</a>.
+                                    By creating an account, you agree to Stuvi's <a href="#" data-toggle="modal"
+                                                                                    data-target=".terms-modal">Term of
+                                        Use</a> and
+                                    <a href="#" data-toggle="modal" data-target=".privacy-modal">Privacy Notice</a>.
                                 </div>
 
                                 <div class="form-group">
@@ -154,6 +156,38 @@
                                 </div>
                             </form>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade terms-modal" tabindex="-1" role="dialog" aria-labelledby="Terms of Use">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close close-modal-btn" data-dismiss="modal"
+                                aria-label="close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        <h3>Terms of Use</h3>
+                    </div>
+                    <div class="modal-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade privacy-modal" tabindex="-1" role="dialog" aria-labelledby="Privacy Notice">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close close-modal-btn" data-dismiss="modal"
+                                aria-label="close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        <h3>Privacy Notice</h3>
+                    </div>
+                    <div class="modal-body">
+
                     </div>
                 </div>
             </div>
