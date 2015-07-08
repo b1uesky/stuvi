7c4876f482d0701ae484ca92076ea5ed5bdfb7cc
B1ueSky <programmer.lty@gmail.com>
2015-07-08 16:54:46 -0400

    Fix remove cart item bug.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -97,8 +97,8 @@ Route::group(['middleware'=>'auth', 'prefix'=>'stripe'], function()
 Route::group(['namespace'=>'Textbook', 'middleware'=>'auth', 'prefix'=>'cart'], function()
 {
     Route::get('/', 'CartController@index');
-    Route::get('add/{cart_id}', 'CartController@addItem');
-    Route::get('rmv/{cart_id}', 'CartController@removeItem');
+    Route::get('add/{id}', 'CartController@addItem');
+    Route::get('rmv/{id}', 'CartController@removeItem');
     Route::get('empty', 'CartController@emptyCart');
     Route::get('update', 'CartController@updateCart');
 });
