686a2e8429ff3e5e53cc1aa0cd2fd3a7fc50c591
kingdido999 <kingdido999@gmail.com>
2015-07-09 16:14:41 -0400

    send sms to courier

--- app/Http/Controllers/Textbook/SellerOrderController.php
+++ app/Http/Controllers/Textbook/SellerOrderController.php
@@ -7,6 +7,7 @@ use App\Address;
 use App\SellerOrder;
 use App\StripeTransfer;
 
+use Log;
 use Auth;
 use Cart;
 use Config;
@@ -19,6 +20,7 @@ use Session;
 use Validator;
 use DateTime;
 use Carbon\Carbon;
+use Aloha\Twilio\Twilio;
 
 class SellerOrderController extends Controller
 {
@@ -77,6 +79,13 @@ class SellerOrderController extends Controller
         if (!is_null($seller_order) && $seller_order->isBelongTo(Auth::id()))
         {
             $seller_order->cancel();
+
+            // if the order is assigned to a courier, send a sms to let the courier know
+            // that the order has been cancelled
+            if ($seller_order->assignedToCourier())
+            {
+                $this->notifyCourierCancelledOrder($seller_order);
+            }
 
             return redirect('order/seller/' . $id);
         }
@@ -313,7 +322,6 @@ class SellerOrderController extends Controller
             // Something else happened, completely unrelated to Stripe
             $error6 = $e->getMessage();
         }
-
     }
 
     /**
@@ -334,4 +342,22 @@ class SellerOrderController extends Controller
         return $url;
     }
 
+    /**
+     * Send a sms to the courier that the order has been cancelled.
+     *
+     * @param $seller_order
+     */
+    protected function notifyCourierCancelledOrder($seller_order)
+    {
+        $twilio = new Twilio(
+            Config::get('twilio.twilio.connections.twilio.sid'),
+            Config::get('twilio.twilio.connections.twilio.token'),
+            Config::get('twilio.twilio.connections.twilio.from')
+        );
+
+        $phone_number = $seller_order->courier->phone_number;
+        $message = 'Order #' . $seller_order->id . ' has been cancelled by the seller at ' . $seller_order->getCancelledTime();
+
+        $twilio->message($phone_number, $message);
+    }
 }
