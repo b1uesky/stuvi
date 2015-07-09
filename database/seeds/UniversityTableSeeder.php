dcdb2c7bae57f0a8aeb21ed88f6a2ac0c083b43e
B1ueSky <programmer.lty@gmail.com>
2015-07-08 22:33:25 -0400

    Add `is_public` field in `universities` table.

--- database/seeds/UniversityTableSeeder.php
+++ database/seeds/UniversityTableSeeder.php
@@ -3,23 +3,47 @@
 use Illuminate\Database\Seeder;
 use App\University;
 
-class UniversityTableSeeder extends Seeder {
-
-public function run()
+class UniversityTableSeeder extends Seeder
 {
-    DB::table('universities')->delete();
 
-    University::create([
-        'name'  => 'Boston University',
-        'abbreviation'  => 'BU',
-        'email_suffix'  => 'bu.edu'
-    ]);
+    public function run()
+    {
+        DB::table('universities')->delete();
 
-    University::create([
-        'name'  => 'Massachusetts Institute of Technology',
-        'abbreviation'  => 'MIT',
-        'email_suffix'  => 'mit.edu'
-    ]);
-}
+        University::create([
+            'name'         => 'Boston University',
+            'abbreviation' => 'BU',
+            'email_suffix' => 'bu.edu',
+            'is_public'    => true,
+        ]);
+
+        University::create([
+            'name'         => 'Massachusetts Institute of Technology',
+            'abbreviation' => 'MIT',
+            'email_suffix' => 'mit.edu',
+            'is_public'    => false,
+        ]);
+
+        University::create([
+            'name'  => 'Northeastern University',
+            'abbreviation'  => 'NEU',
+            'email_suffix'  => 'neu.edu',
+            'is_public'     => false,
+        ]);
+
+        University::create([
+            'name'  => 'Harvard University',
+            'abbreviation'  => 'HARVARD',
+            'email_suffix'  => 'harvard.edu',
+            'is_public'     => false,
+        ]);
+
+        University::create([
+            'name'  => 'Boston College',
+            'abbreviation'  => 'BC',
+            'email_suffix'  => 'bc.edu',
+            'is_public'     => false,
+        ]);
+    }
 
 }
