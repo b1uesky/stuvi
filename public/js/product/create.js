47f73c5dc43091943d595471bb1b59e6415437e2
kingdido999 <kingdido999@gmail.com>
2015-07-11 17:02:25 -0400

    some comments

--- public/js/product/create.js
+++ public/js/product/create.js
@@ -1,5 +1,7 @@
 /**
  * Created by Desmond on 7/11/15.
+ *
+ * Page: /textbook/sell/product/{id}/create
  */
 
 var total_images_limit = 3;
@@ -9,20 +11,22 @@ $(document).ready(function () {
     // generate an extra input element for image upload
     $('.btn-add-input').click(function () {
 
-        // disable add image if the limit is reached
+        // disable add image button if the limit is reached
         if ($('.upload-file').length == total_images_limit - 1 ) {
             $('.btn-add-input').addClass('disabled');
         }
 
+        // add input field
         if ($('.upload-file').length < total_images_limit) {
             var html_block = [
                 '<div class="form-group">',
                 '<input type="file" name="extra-image" class="upload-file" />',
                 '<div class="upload-error-message">The file size is too large. Please make sure the file size is under 3MB.</div>',
-                '<a class="btn btn-danger btn-remove-file">Remove</a>',
+                '<a class="btn btn-danger btn-remove-file">Delete</a>',
                 '</div>'
             ];
 
+            // insert the block before submit button
             $(html_block.join('')).insertBefore('input[name=submit]');
         }
 
@@ -30,10 +34,13 @@ $(document).ready(function () {
 
 });
 
-// remove a dynamically created input field and reenable add image button
+// click event for delete button
 $(document).on('click', '.btn-remove-file', function() {
 
+    // remove a dynamically created input field
     $(this).parent().remove();
+
+    // reenable add image button
     $('.btn-add-input').removeClass('disabled');
 
 });
