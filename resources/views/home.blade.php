74204053a20cac334d52a0b7144593fcf61f1ba8
patels95 <patels326@gmail.com>
2015-07-17 16:56:25 -0400

    home page navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 <!-- Prototype Homepage Copyright Stuvi 2015 -->
 
-@extends('app')    <!-- app.blade.php -->
+@extends('app-home')    <!-- app.blade.php -->
 
 @section('title', 'Textbooks, Housing, Clubs, & More')
 
@@ -20,6 +20,79 @@
     --}}
 
     <div class = "container-fluid container-top backgnd">           <!-- Top half -->
+        {{--navigation--}}
+        <nav class="navbar navbar-default" id="nav" role="navigation">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <!-- Toggle Nav into hamburger menu for small screens -->
+                    <button id="nav-toggle-collapse" type="button" class="navbar-toggle collapsed"
+                            data-toggle="collapse"
+                            data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <i class="fa fa-bars fa-lg"></i>
+                    </button>
+                    <div class="logo-container">
+                        <a href="{{url('/home')}}"> <img src="{{asset('/img/stuvi-logo.png')}}" class="img-responsive">
+                        </a>
+                    </div>
+                </div>
+                <!-- End Navbar header -->
+
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <!-- Nav Bar Links-->
+                    <ul class="nav navbar-nav" id="nav-left">
+                        <li><a href="{{ url('/textbook') }}" class="" id="textbook-nav">Textbook</a></li>
+                        {{--<li><a href="{{ url('/coming') }}">Housing</a></li>--}}
+                        {{--<li><a href="{{ url('/coming') }}">Club</a></li>--}}
+                        {{--<li><a href="{{ url('/coming') }}">Group</a></li>--}}
+                    </ul>
+                    <!-- Navbar right -->
+                    <ul id="nav-right" class="nav navbar-nav navbar-right">
+                        @if (Auth::guest())
+                            <li><a id="login-btn" class="nav-login" href="{{ url('/auth/login') }}">
+                                    <i class="fa fa-sign-in"></i> Login</a></li>     <!-- added font awesome icons -->
+                            <li><a id="register-btn" class="nav-login" href="{{ url('/auth/register') }}">
+                                    <i class="fa fa-user"></i> Sign Up</a></li>
+                            @else
+                                    <!-- profile dropdown -->
+                            <li class="dropdown" id="dp">
+                                <a href="#" id="nav-drop" class="dropdown-toggle nav-dropdown" data-toggle="dropdown"
+                                   role="button"
+                                   aria-expanded="true"><span nav-caret
+                                                              id="account-name">{{ Auth::user()->first_name }} </span><span
+                                            class="caret nav-caret"></span></a>
+                                <ul class="dropdown-menu" id="nav-dropdown" role="menu" aria-labelledby="nav-dropdown">
+                                    <li role="presentation"><a role="menuitem" tabindex="-1"
+                                                               href="{{ url('/user/profile') }}">
+                                            Profile</a></li>
+                                    <li role="presentation"><a role="menuitem" tabindex="-1"
+                                                               href="{{ url('/user/account') }}">
+                                            Your Account</a></li>
+                                    <li role="presentation"><a role="menuitem" tabindex="-1"
+                                                               href="{{ url('/order/buyer') }}">
+                                            Your Orders</a></li>
+                                    <li role="presentation"><a role="menuitem" tabindex="-1"
+                                                               href="{{ url('/order/seller') }}">
+                                            Sold Books</a></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li role="presentation"><a role="menuitem" tabindex="-1"
+                                                               href="{{ url('/auth/logout') }}">
+                                            Logout</a></li>
+                                </ul>
+                            </li>
+                            <!-- cart -->
+                            <li class="cart">
+                                <a href="{{ url('/cart') }}" id="cart-link">Cart <i class="fa fa-shopping-cart"
+                                                                                    style="line-height: 19px;"></i></a>
+                            </li>
+                        @endif
+
+                    </ul>
+                </div>
+                <!-- End collapse container -->
+            </div>
+            <!-- End navbar container -->
+        </nav>
         <div class="container top-content col-md-12">
             <h1 id="head1">Welcome to Stuvi</h1>
             <p class="lead tagline">Because it takes a village to conquer college.</p>
