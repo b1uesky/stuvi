a0a2bc39de72f1fdb328073b683fe7383c4bf4ba
kingdido999 <kingdido999@gmail.com>
2015-07-17 10:25:55 -0400

    update seeder

--- database/seeds/AddressTableSeeder.php
+++ database/seeds/AddressTableSeeder.php
@@ -17,14 +17,12 @@ class AddressTableSeeder extends Seeder
     {
         DB::table('addresses')->delete();
 
-        $buyer = User::where('email', '=', 'buyer@stuvi.com')->first();
-
         $faker = Factory::create();
 
-         for ($i = 0; $i < 10; $i++)
+         for ($i = 0; $i < 30; $i++)
          {
              Address::create(array(
-                 'user_id' => $buyer->id,
+                 'user_id' => $faker->numberBetween(1, User::count()),
                  'is_default' => false,
                  'addressee' => $faker->name,
                  'address_line1' => $faker->streetAddress,
@@ -38,6 +36,24 @@ class AddressTableSeeder extends Seeder
                  'phone_number' => $faker->phoneNumber
              ));
          }
+
+        for ($i = 0; $i < 5; $i++)
+        {
+            Address::create(array(
+                'user_id' => 1,
+                'is_default' => false,
+                'addressee' => $faker->name,
+                'address_line1' => $faker->streetAddress,
+                'address_line2' => $faker->secondaryAddress,
+                'city' => $faker->city,
+                'state_a2' => $faker->stateAbbr,
+                'state_name' => $faker->state,
+                'zip' => $faker->postcode,
+                'country_a2' => 'US',
+                'country_name' => 'United States of America',
+                'phone_number' => $faker->phoneNumber
+            ));
+        }
 
         Address::find(1)->update([
             'is_default' => true
