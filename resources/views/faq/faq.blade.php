7a9e68b6f4f82226acc8c45db69572abb0a98bf3
patels95 <patels326@gmail.com>
2015-07-23 12:58:16 -0400

    updated general faq page

--- resources/views/faq/faq.blade.php
+++ resources/views/faq/faq.blade.php
@@ -7,7 +7,47 @@
 @endsection
 
 @section('content')
+    <div class="container faq-container">
+        <div class="col-sm-3 faq-links-container">
+            <h4>General Questions</h4>
+            <ul class="faq-list">
+                <li>
+                    <a>Why do I need to log in with a school email address?</a>
+                </li>
+            </ul>
+            <h4>Textbook</h4>
+            <ul class="faq-list">
+                <li>
+                    <a></a>
+                </li>
+            </ul>
+            <h4>Orders</h4>
+            <ul class="faq-list">
+                <li>
+                    <a>What happens after I buy a book?</a>
+                </li>
+                <li>
+                    <a>What happens when someone buys my book?</a>
+                </li>
+            </ul>
+            <h4>Account</h4>
+            <ul class="faq-list">
+                <li>
+                    <a>How do I edit my account information?</a>
+                </li>
+                <li>
+                    <a>Change I change the privacy of my profile?</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-sm-7 col-sm-offset-1 general-container">
+            <h3>Why do I need to log in with a school email address?</h3>
 
+            <p>
+
+            </p>
+        </div>
+    </div>
 @endsection
 
 @section('javascript')
