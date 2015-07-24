998f5bf2da391f422e7055f3f009207135370913
nlouie <louie.nick@yahoo.com>
2015-07-24 10:28:27 -0400

    slider changes

--- public/js/home.js
+++ public/js/home.js
@@ -13,7 +13,7 @@ $(function(){
         //},
         effect: {
             fade: {
-                speed: 600
+                speed: 1000
             }
         },
         play: {
@@ -22,7 +22,7 @@ $(function(){
             // You cannot use your own buttons. Sorry.
             effect: "fade",
             // [string] Can be either "slide" or "fade".
-            interval: 5000,
+            interval: 7000,
             // [number] Time spent on each slide in milliseconds.
             auto: true,
             // [boolean] Start playing the slideshow on load.
