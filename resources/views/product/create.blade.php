80decbe0b2778dc754dae1e74cb7a99c6238f211
B1ueSky <programmer.lty@gmail.com>
2015-07-24 11:17:28 -0400

    Update create product template.

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -52,10 +52,10 @@
 
         {{-- If the user is not logged in, show login / signup buttons. --}}
         @if(!Auth::check())
-            <p>You need to login or sign up to continue using our sevice.</p>
             <div class="row col-sm-6 col-sm-offset-1">
+                <p>Please login or sign up to continue using our service.</p>
                 <a href="{{ url('textbook/sell/product/login') }}" class="btn btn-default">Login</a>
-                <a href="{{ url('textbook/sell/product/register') }}" class="btn btn-default">Signup</a>
+                <a href="{{ url('textbook/sell/product/register') }}" class="btn btn-default">Sign up</a>
             </div>
         @else
             {{-- Show book conditions --}}
@@ -82,7 +82,7 @@
                                             aria-label="close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
-                                    <h3>General Conditions</h3>
+                                    <h3>{{ Config::get('product.conditions.general_condition.title') }}</h3>
                                 </div>
                                 <div class="modal-body">
                                     <h4>Brand New</h4>
@@ -136,7 +136,7 @@
                                             aria-label="close">
                                         <span id="close-span" aria-hidden="true">&times;</span>
                                     </button>
-                                    <h3>Highlights/Notes</h3>
+                                    <h3>{{ Config::get('product.conditions.highlights_and_notes.title') }}</h3>
                                 </div>
                                 <div class="modal-body">
                                     <p>{{ Config::get('product.conditions.highlights_and_notes.description') }}</p>
@@ -175,7 +175,7 @@
                                             aria-label="close">
                                         <span id="close-span" aria-hidden="true">&times;</span>
                                     </button>
-                                    <h3>Damaged Pages</h3>
+                                    <h3>{{ Config::get('product.conditions.damaged_pages.title') }}</h3>
                                 </div>
                                 <div class="modal-body">
                                     <p>{{ Config::get('product.conditions.damaged_pages.description') }}</p>
@@ -214,7 +214,7 @@
                                             aria-label="close">
                                         <span id="close-span" aria-hidden="true">&times;</span>
                                     </button>
-                                    <h3>Broken Binding</h3>
+                                    <h3>{{ Config::get('product.conditions.broken_binding.title') }}</h3>
                                 </div>
                                 <div class="modal-body">
                                     <p>{{ Config::get('product.conditions.broken_binding.description') }}</p>
