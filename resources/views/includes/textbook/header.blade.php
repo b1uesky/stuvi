1b74e60e37ae40794fa086ecfa1feb8fbc228c0f
patels95 <patels326@gmail.com>
2015-07-27 12:36:14 -0400

    updated mobile dropdown

--- resources/views/includes/textbook/header.blade.php
+++ resources/views/includes/textbook/header.blade.php
@@ -19,7 +19,7 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <!-- Nav Bar Links-->
                 <ul class="nav navbar-nav" id="nav-left">
-                    <li><a href="{{ url('/textbook') }}" class="" id="textbook-nav">Textbooks</a></li>
+                    <li><a href="{{ url('/textbook') }}" id="textbook-nav">Textbooks</a></li>
                     {{--<li><a href="{{ url('/coming') }}">Housing</a></li>--}}
                     {{--<li><a href="{{ url('/coming') }}">Club</a></li>--}}
                     {{--<li><a href="{{ url('/coming') }}">Group</a></li>--}}
