fb8b5d9287b08a390f96fe3f9619952e84a873d0
nlouie <louie.nick@yahoo.com>
2015-07-28 12:19:48 -0400

    img

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -22,10 +22,10 @@
                             <!-- row 1 header-->
                             <tr>
                                 <!-- header image/logo -->
-                                <td align="center" bgcolor="#241729" style="padding: 40px 0 30px 0; color: #153643; font-size: 28px; font-weight: bold; font-family: Trebuchet MS, Helvetica, sans-serif;">
+                                <td align="center" bgcolor="#f2f2f2" style="padding: 40px 0 30px 0; color: #153643; font-size: 28px; font-weight: bold; font-family: Trebuchet MS, Helvetica, sans-serif;">
                                     <!--TODO: puush must be replaced with a real url once website is online -->
                                     <a href="{{url('/home')}}">
-                                        <img src="http://puu.sh/iDvTG/18b055e116.png" alt="Stuvi" width="276" height="110" style="display: block; color: #ffffff" />
+                                        <img src="http://puu.sh/jg7HJ/cbdfb5e1f5.png" alt="Stuvi" width="276" height="110" style="display: block; color: #ffffff" />
                                     </a>
                                 </td> <!-- end header img -->
                             </tr>
