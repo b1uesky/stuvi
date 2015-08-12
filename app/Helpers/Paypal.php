24d8097a4bece1bd8a34f3858624ae0f7a49754c
kingdido999 <kingdido999@gmail.com>
2015-08-12 14:12:49 -0400

    add createPaymentByPaypal sample

--- app/Helpers/Paypal.php
+++ app/Helpers/Paypal.php
@@ -155,6 +155,83 @@ class Paypal extends Payment
         return $payment;
     }
 
+
+    public function createPaymentByPaypal()
+    {
+        // ### Payer
+        // A resource representing a Payer that funds a payment
+        // For paypal account payments, set payment method
+        // to 'paypal'.
+        $payer = Paypalpayment::payer();
+        $payer->setPaymentMethod("paypal");
+
+        // ### Itemized information
+        // (Optional) Lets you specify item wise
+        // information
+        $item1 = Paypalpayment::item();
+        $item1->setName('Ground Coffee 40 oz')
+            ->setCurrency('USD')
+            ->setQuantity(1)
+            ->setSku("123123")// Similar to `item_number` in Classic API
+            ->setPrice(7.5);
+
+        $item2 = Paypalpayment::item();
+        $item2->setName('Granola bars')
+            ->setCurrency('USD')
+            ->setQuantity(5)
+            ->setSku("321321")// Similar to `item_number` in Classic API
+            ->setPrice(2);
+
+        $itemList = Paypalpayment::itemList();
+        $itemList->setItems(array($item1, $item2));
+
+        // ### Additional payment details
+        // Use this optional field to set additional
+        // payment information such as tax, shipping
+        // charges etc.
+        $details = Paypalpayment::details();
+        $details->setShipping(1.2)
+            ->setTax(1.3)
+            ->setSubtotal(17.50);
+
+        // ### Amount
+        // Lets you specify a payment amount.
+        // You can also specify additional details
+        // such as shipping, tax.
+        $amount = Paypalpayment::amount();
+        $amount->setCurrency("USD")
+            ->setTotal(20)
+            ->setDetails($details);
+
+        // ### Transaction
+        // A transaction defines the contract of a
+        // payment - what is the payment for and who
+        // is fulfilling it.
+        $transaction = Paypalpayment::transaction();
+        $transaction->setAmount($amount)
+            ->setItemList($itemList)
+            ->setDescription("Payment description")
+            ->setInvoiceNumber(uniqid());
+
+        // ### Redirect urls
+        // Set the urls that the buyer must be redirected to after
+        // payment approval/ cancellation.
+        $baseUrl = getBaseUrl();
+        $redirectUrls = Paypalpayment::redirectUrls();
+        $redirectUrls
+            ->setReturnUrl("$baseUrl/ExecutePayment.php?success=true")
+            ->setCancelUrl("$baseUrl/ExecutePayment.php?success=false");
+
+        // ### Payment
+        // A Payment Resource; create one using
+        // the above types and intent set to 'sale'
+        $payment = Paypalpayment::payment();
+        $payment->setIntent("sale")
+            ->setPayer($payer)
+            ->setRedirectUrls($redirectUrls)
+            ->setTransactions(array($transaction));
+    }
+
     /**
      * Validation rules.
      *
