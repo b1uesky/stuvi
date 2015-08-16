04eb2ba2154f909b4f5c6a94dfb0ec6d989d6974
kingdido999 <kingdido999@gmail.com>
2015-08-16 19:15:57 -0400

    footer redesign

--- resources/views/includes/textbook/footer.blade.php
+++ resources/views/includes/textbook/footer.blade.php
@@ -1,48 +1,75 @@
-<!-- FOOTER. See footer styling at footer-distributed.css -->
-
 <footer class="footer-distributed">
-    {{--<div class="row">
-        <hr style="    border: 0;height: 2px;background: #333; background-image: linear-gradient(to right, #ccc, #f16521, #ccc);">
-    </div>--}}
 
-    <!-- Social Media -->
-    <div class="footer-right">
-        <!-- Uses font-awesome.css -->
-        <a class="social" href="https://www.facebook.com/StuviBoston" target="_blank" name="facebook">
-            <i class="fa fa-facebook"></i>
-        </a>
-        <a class="social" href="https://twitter.com/StuviBoston" target="_blank" name="twitter">
-            <i class="fa fa-twitter"></i>
-        </a>
-        <a class="social" href="https://www.linkedin.com/company/stuvi" target="_blank" name="linkedin">
-            <i class="fa fa-linkedin"></i>
-        </a>
+    <div class="container">
+        <div class="row">
+            <ul class="footer-links col-xs-4 text-left">
+                <li><a href="{{ url('/about') }}" name="about">About</a></li>
+                <li><a href="{{ url('/faq/general') }}" name="FAQ">FAQ</a></li>
+                <li class="nobullet"><a href="{{ url('/contact') }}" name="contact">Contact</a></li>
+                {{--<a href="{{ url('/sitemap') }}" name="sitemap">Sitemap</a>--}}
+            </ul>
+
+
+            <div class="social-links col-xs-4 text-center">
+                <!-- Uses font-awesome.css -->
+                <a href="https://www.facebook.com/StuviBoston" target="_blank" name="facebook">
+                    <i class="fa fa-facebook"></i>
+                </a>
+                <a href="https://twitter.com/StuviBoston" target="_blank" name="twitter">
+                    <i class="fa fa-twitter"></i>
+                </a>
+                <a href="https://www.linkedin.com/company/stuvi" target="_blank" name="linkedin">
+                    <i class="fa fa-linkedin"></i>
+                </a>
+            </div>
+
+            <ul class="company-info col-xs-4 text-right">
+                <li><span class="copyright">&copy; 2015 Stuvi</span></li>
+                <li><a href="#" name="terms">Terms</a></li>
+                <li class="nobullet"><a href="#" name="privacy">Privacy</a></li>
+            </ul>
+        </div>
+
     </div>
 
-    <div class="footer-left">
+    {{--<!-- Social Media -->--}}
+    {{--<div class="footer-right">--}}
+        {{--<!-- Uses font-awesome.css -->--}}
+        {{--<a class="social" href="https://www.facebook.com/StuviBoston" target="_blank" name="facebook">--}}
+            {{--<i class="fa fa-facebook"></i>--}}
+        {{--</a>--}}
+        {{--<a class="social" href="https://twitter.com/StuviBoston" target="_blank" name="twitter">--}}
+            {{--<i class="fa fa-twitter"></i>--}}
+        {{--</a>--}}
+        {{--<a class="social" href="https://www.linkedin.com/company/stuvi" target="_blank" name="linkedin">--}}
+            {{--<i class="fa fa-linkedin"></i>--}}
+        {{--</a>--}}
+    {{--</div>--}}
 
-        <p class="footer-links">
-            <a class="footer-link" href="{{url('/home')}}" name="home">Home</a>
-            ·
-            <a class="footer-link"  href="{{url('/textbook')}}" name="textbooks">Textbooks</a>
-{{--            ·
-            <a class="footer-link"  href="{{url('/coming')}}">Housing</a>
-            ·
-            <a class="footer-link" href="{{url('/coming')}}">Clubs</a>
-            ·
-            <a class="footer-link" href="{{url('/coming')}}">Groups</a>--}}
-            ·
-            <a class="footer-link"  href="{{ url('/about') }}" name="about">About</a>
-            ·
-            <a class="footer-link"  href="{{ url('/contact') }}" name="contact">Contact</a>
-            ·
-            <a class="footer-link" href="{{ url('/faq/general') }}" name="FAQ">FAQ</a>
-            ·
-            <a class="footer-link" href="{{ url('/sitemap') }}" name="sitemap">Sitemap</a>
-        </p>
-        <hr>
-        <p>&copy; Stuvi, LLC. 2015</p>
-    </div>
+    {{--<div class="footer-left">--}}
+
+        {{--<p class="footer-links">--}}
+            {{--<a class="footer-link" href="{{url('/home')}}" name="home">Home</a>--}}
+            {{--·--}}
+            {{--<a class="footer-link"  href="{{url('/textbook')}}" name="textbooks">Textbooks</a>--}}
+{{--            ·--}}
+            {{--<a class="footer-link"  href="{{url('/coming')}}">Housing</a>--}}
+            {{--·--}}
+            {{--<a class="footer-link" href="{{url('/coming')}}">Clubs</a>--}}
+            {{--·--}}
+            {{--<a class="footer-link" href="{{url('/coming')}}">Groups</a>--}}
+            {{--·--}}
+            {{--<a class="footer-link"  href="{{ url('/about') }}" name="about">About</a>--}}
+            {{--·--}}
+            {{--<a class="footer-link"  href="{{ url('/contact') }}" name="contact">Contact</a>--}}
+            {{--·--}}
+            {{--<a class="footer-link" href="{{ url('/faq/general') }}" name="FAQ">FAQ</a>--}}
+            {{--·--}}
+            {{--<a class="footer-link" href="{{ url('/sitemap') }}" name="sitemap">Sitemap</a>--}}
+        {{--</p>--}}
+        {{--<hr>--}}
+        {{--<p>&copy; Stuvi, LLC. 2015</p>--}}
+    {{--</div>--}}
 
 </footer>
 <!-- END FOOTER -->
