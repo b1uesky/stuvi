46319eb576141ab6371b510af389c381448bd8e7
B1ueSky <programmer.lty@gmail.com>
2015-08-17 13:02:43 -0400

    Add cover image on admin book index page.

--- resources/views/admin/book/index.blade.php
+++ resources/views/admin/book/index.blade.php
@@ -6,28 +6,30 @@
     <table class="table table-hover">
         <tr>
             <th>ID</th>
+            <th>Cover</th>
             <th>Title</th>
             <th>Edition</th>
             <th>ISBN10</th>
             <th>ISBN13</th>
-            <th>Number of Pages</th>
             <th>Verified</th>
-            <th>Language</th>
-            <th>Created At</th>
             <th>Actions</th>
         </tr>
 
         @foreach($books as $book)
             <tr>
                 <td>{{ $book->id }}</td>
+                <td>
+                @if($book->imageSet->small_image)
+                    <img class="admin-img-preview" alt="" src="{{ config('aws.url.stuvi-book-img') . $book->imageSet->small_image }}">
+                @else
+                    <img class="admin-img-preview" alt="" src="{{ config('book.default_image_path.small')}}">
+                @endif
+                </td>
                 <td>{{ $book->title }}</td>
                 <td>{{ $book->edition }}</td>
                 <td>{{ $book->isbn10 }}</td>
                 <td>{{ $book->isbn13 }}</td>
-                <td>{{ $book->num_pages }}</td>
                 <td>{{ $book->is_verified }}</td>
-                <td>{{ $book->language }}</td>
-                <td>{{ $book->created_at }}</td>
                 <td><a class="btn btn-default" role="button" href="{{ URL::to('admin/book/' . $book->id) }}">Details</a></td>
             </tr>
         @endforeach
