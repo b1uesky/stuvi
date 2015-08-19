7a35d68b55a3bb7044ac7fc6ce8f280710a2e184
B1ueSky <programmer.lty@gmail.com>
2015-08-19 17:11:25 -0400

    Add filter function in admin seller order page.

--- app/Http/Controllers/Admin/SellerOrderController.php
+++ app/Http/Controllers/Admin/SellerOrderController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests;
 use App\SellerOrder;
 use Config;
+use Input;
 
 class SellerOrderController extends Controller
 {
@@ -16,9 +17,57 @@ class SellerOrderController extends Controller
      */
     public function index()
     {
-        $seller_orders = SellerOrder::orderBy('id', 'DESC')->paginate(Config::get('pagination.limit.admin.seller_order'));
+        $filter   = Input::get('filter');
+        $keyword  = strtolower(Input::get('keyword'));
+        $order_by = Input::get('order_by', 'id');
+        $order    = Input::get('order', 'DESC');
 
-        return view('admin.sellerOrder.index')->withSellerOrders($seller_orders);
+        // filter
+        if (empty($keyword))
+        {
+            $query = SellerOrder::query();
+        }
+        elseif ($filter == 'id')
+        {
+            $query = SellerOrder::where($filter, intval($keyword));
+        }
+        elseif ($filter == 'title')
+        {
+            $query = SellerOrder::buildQueryWithBookTitle($keyword);
+        }
+        elseif ($filter == 'seller')
+        {
+            $query = SellerOrder::buildQueryWithSellerName($keyword);
+        }
+        else
+        {
+            $query = SellerOrder::query();
+        }
+
+        // order
+        if ($order_by == 'title')
+        {
+            $query = $query->join('products', 'seller_orders.product_id', '=', 'products.id')
+                           ->join('books', 'products.book_id', '=', 'books.id')
+                           ->select('seller_orders.*', 'books.title');
+        }
+        elseif ($order_by == 'first_name' || $order_by == 'last_name')
+        {
+            $query = $query->join('products', 'seller_orders.product_id', '=', 'products.id')
+                           ->join('users', 'products.seller_id', '=', 'users.id')
+                           ->select('seller_orders.*', 'users.'.$order_by);
+        }
+
+        $seller_orders = $query->orderBy($order_by, $order)->paginate(Config::get('pagination.limit.admin.seller_order'));
+
+        return view('admin.sellerOrder.index')->withSellerOrders($seller_orders)
+            ->with('pagination_params', Input::only([
+                                                        'filter',
+                                                        'keyword',
+                                                        'order_by',
+                                                        'order',
+                                                        'page',
+                                                    ]));
     }
 
     /**
