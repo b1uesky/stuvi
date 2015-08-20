d0341236baa727196da8f3d6e1b101e977496d99
kingdido999 <kingdido999@gmail.com>
2015-08-20 09:52:21 -0400

    display paypal account correctly in product create page

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -237,7 +237,7 @@
                 {{-- Paypal account --}}
                 <div class="form-group">
                     <label>Paypal Account</label>
-                    <input type="email" name="paypal" class="form-control" value="{{ Auth::user()->profile->paypal or '' }}" placeholder="Paypal email address">
+                    <input type="email" name="paypal" class="form-control" value="{{ $paypal or '' }}" placeholder="Paypal email address">
                 </div>
 
                 <button type="submit" name="submit" class="btn primary-btn sell-btn">Post Book</button>
