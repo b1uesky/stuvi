5876d158eff24ff0f3fb324ec195ed04e7841137
nlouie <louie.nick@yahoo.com>
2015-08-21 15:40:21 -0400

    style changes.

--- resources/views/user/bookshelf.blade.php
+++ resources/views/user/bookshelf.blade.php
@@ -17,7 +17,8 @@
             @include('includes.textbook.flash-message')
 
             <div class="bookshelf-title-container">
-                <h1 id="bookshelf-title">Your Bookshelf <small>Your books for sale</small></h1>
+                <h1 id="bookshelf-title">Your Bookshelf</h1>
+                <h4 id="bookshelf-title-subtext">Your books for Sale</h4>
             </div>
             <!-- books -->
             <div class="container col-sm-11 col-md-12 for-sale-table-container">
