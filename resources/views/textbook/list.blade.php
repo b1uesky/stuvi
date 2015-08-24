5785e33890a927093fa82fceeda59a177609f04d
kingdido999 <kingdido999@gmail.com>
2015-08-24 16:03:15 -0400

    styling textbook list page

--- resources/views/textbook/list.blade.php
+++ resources/views/textbook/list.blade.php
@@ -16,85 +16,100 @@
 
     <div class="container-fluid textbook-list-container">
 
-        <div class="container textbook-list">
-            {{-- HEADER --}}
-            <div class="textbook-list-header">
-                <h2 class="textbook-list-title">Books</h2>
-
-                @if(trim($query) != "")
-                    <div class="textbook-list-results">
-                        {{ count($books) }} results for {{ $query }}
-                    </div>
-                @else
-                    <div class="textbook-list-results">
-                        {{ count($books) }} results
-                    </div>
-                @endif
+        <div class="container">
+            <div class="page-header">
+                <h1>Books</h1>
             </div>
 
-            <table class="table table-responsive textbook-table">
-                <!-- new row for each book -->
-                @forelse($books as $book)
-                    <tr class="textbook-item">
-                        <td class="textbook-img-container">
-                            <a href="{{ url("textbook/buy/".$book->id) }}">
+            {{--@if(trim($query) != "")--}}
+            {{--<div class="textbook-list-results">--}}
+            {{--{{ count($books) }} results for {{ $query }}--}}
+            {{--</div>--}}
+            {{--@else--}}
+            {{--<div class="textbook-list-results">--}}
+            {{--{{ count($books) }} results--}}
+            {{--</div>--}}
+            {{--@endif--}}
 
-                                @if($book->imageSet->small_image)
-                                    <img class="textbook-img" src="{{ config('aws.url.stuvi-book-img') . $book->imageSet->small_image }}">
-                                @else
-                                    <img class="textbook-img" src="{{ config('book.default_image_path.small')}}">
-                                @endif
-                            </a>
-                        </td>
-                        <td class="textbook-info-1">
-                            <span class="textbook-title"><a href="{{ url("textbook/buy/".$book->id) }}">{{ $book->title }}</a></span><br>
 
-                            @if($book->authors->count())
-                                <span>Author(s):</span>
-                                <?php $i = 0; ?>
-                                @foreach($book->authors as $author)
-                                    @if($i == 0)
-                                        <span>{{ $author->full_name }}</span>
-                                        <?php $i++ ?>
+            @forelse($books as $book)
+                <div class="margin-large border-bottom">
+                    <table class="table table-no-border">
+                        <tr>
+                            <td class="col-xs-2">
+                                <a href="{{ url("textbook/buy/".$book->id) }}">
+                                    @if($book->imageSet->small_image)
+                                        <img class="img-responsive"
+                                             src="{{ config('aws.url.stuvi-book-img') . $book->imageSet->small_image }}">
                                     @else
-                                        <span>, {{ $author->full_name }}</span>
+                                        <img class="img-responsive" src="{{ config('book.default_image_path.small')}}">
                                     @endif
-                                @endforeach
-                            @endif
+                                </a>
+                            </td>
 
-                            <br>
-                            <span class="textbook-isbn">ISBN10: {{ $book->isbn10 }}</span>
-                            <br>
-                            <span class="textbook-isbn">ISBN13: {{ $book->isbn13 }}</span>
-                            <br>
-                            <br>
+                            <td class="col-xs-10">
+                                <table class="table-details full-width rel-bot-10">
+                                    <tr>
+                                        <td class="col-xs-6">
+                                            <h4><a href="{{ url("textbook/buy/".$book->id) }}">{{ $book->title }}</a></h4>
+                                        </td>
+                                        <td class="col-xs-4 text-right">
+                                            @if(count($book->products) > 1)
+                                                <span class="price">${{ $book->decimalLowestPrice() }}</span>
+                                                <span class="text-muted"> ~ </span>
+                                                <span class="price">${{ $book->decimalHighestPrice() }}</span>
+                                            @else
+                                                <span class="price">${{ $book->decimalLowestPrice() }}</span>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                </table>
 
-                            <span>
-                                @if(count($book->products) > 1)
-                                    From <span class="textbook-price">${{ $book->decimalLowestPrice() }}</span>
-                                    to <span class="textbook-price">${{ $book->decimalHighestPrice() }}</span>
-                                @else
-                                    <span class="textbook-price">${{ $book->decimalLowestPrice() }}</span>
-                                @endif
-                            </span>
-                        </td>
-                        <td class="table-offset"></td>
-                        <td class="textbook-info-2">
-                            <!-- each class the book support -->
-                            {{--<h5>Classes</h5>--}}
-                            {{--<span class="textbook-class"><a href="#">BU:SMG SM131</a></span>--}}
-                        </td>
-                    </tr>
+                                <table class="table-details full-width">
+                                    <tbody>
+                                    <tr>
+                                        <th class="col-xs-2">
+                                            Author(s):
+                                        </th>
+                                        <td class="col-xs-10">
+                                            @foreach($book->authors as $i => $author)
+                                                @if($i == 0)
+                                                    <span>{{ $author->full_name }}</span>
+                                                @else
+                                                    <span>, {{ $author->full_name }}</span>
+                                                @endif
+                                            @endforeach
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <th class="col-xs-2">ISBN-10:</th>
+                                        <td class="col-xs-10">{{ $book->isbn10 }}</td>
+                                    </tr>
+
+                                    <tr>
+                                        <th class="col-xs-2">ISBN-13:</th>
+                                        <td class="col-xs-10">{{ $book->isbn13 }}</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+
                 @empty
-                    <br>
-                    <p class="empty">Sorry, there are no search results matching "<i>{{ $query }}</i>."</p>
+                    <h3 class="text-center">Sorry, there are no search results matching "<i>{{ $query }}</i>."</h3>
                 @endforelse
-            </table>
-        </div>
-            <div id="pagination">
-            {!! $books->appends(Request::only('query'))->render() !!}
-            </div>
 
+        </div>
+
+
+    </div>
+
+    <div id="pagination">
+        {!! $books->appends(Request::only('query'))->render() !!}
     </div>
 @endsection
 
