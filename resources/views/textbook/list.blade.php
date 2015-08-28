f791fa726bd810e793a09bfb14b81628a90bf708
kingdido999 <kingdido999@gmail.com>
2015-08-28 17:02:08 -0400

    enhance textbook list page responsive design #113

--- resources/views/textbook/list.blade.php
+++ resources/views/textbook/list.blade.php
@@ -18,70 +18,60 @@
             </div>
 
             @forelse($books as $book)
-                <div class="margin-15 border-bottom">
-                    <table class="table table-no-border">
-                        <tr>
-                            <td class="col-xs-3 col-sm-2">
-                                <a href="{{ url("textbook/buy/".$book->id) }}">
-                                    @if($book->imageSet->small_image)
-                                        <img class="img-responsive"
-                                             src="{{ config('aws.url.stuvi-book-img') . $book->imageSet->small_image }}">
-                                    @else
-                                        <img class="img-responsive" src="{{ config('book.default_image_path.small')}}">
-                                    @endif
-                                </a>
-                            </td>
+                <div class="row padding-15 border-bottom">
+                    <div class="col-md-2 col-sm-2 col-xs-4">
+                        <a href="{{ url("textbook/buy/".$book->id) }}">
+                            @if($book->imageSet->small_image)
+                                <img class="img-responsive"
+                                     src="{{ config('aws.url.stuvi-book-img') . $book->imageSet->small_image }}">
+                            @else
+                                <img class="img-responsive" src="{{ config('book.default_image_path.small')}}">
+                            @endif
+                        </a>
+                    </div>
 
-                            <td class="col-xs-9 col-sm-10">
-                                <table class="table-details full-width rel-bot-10">
-                                    <tr>
-                                        <td class="col-sm-8 col-xs-12">
-                                            <h4><a href="{{ url("textbook/buy/".$book->id) }}">{{ $book->title }}</a></h4>
-                                        </td>
-                                        <td class="col-sm-4 text-right hidden-xs">
-                                            @if(count($book->products) > 1)
-                                                <span class="price">${{ $book->decimalLowestPrice() }}</span>
-                                                <span class="text-muted"> ~ </span>
-                                                <span class="price">${{ $book->decimalHighestPrice() }}</span>
-                                            @else
-                                                <span class="price">${{ $book->decimalLowestPrice() }}</span>
-                                            @endif
-                                        </td>
-                                    </tr>
-                                </table>
+                    <div class="col-md-8 col-sm-7 col-xs-8">
+                        <h4 class="col-sm-12 no-margin-top"><a href="{{ url("textbook/buy/".$book->id) }}">{{ $book->title }}</a></h4>
 
-                                <table class="table-details full-width">
-                                    <tbody>
-                                    <tr>
-                                        <th class="col-xs-2">
-                                            Author(s):
-                                        </th>
-                                        <td class="col-xs-10">
-                                            @foreach($book->authors as $i => $author)
-                                                @if($i == 0)
-                                                    <span>{{ $author->full_name }}</span>
-                                                @else
-                                                    <span>, {{ $author->full_name }}</span>
-                                                @endif
-                                            @endforeach
-                                        </td>
-                                    </tr>
+                        <table class="table-details full-width">
+                            <tbody>
+                            <tr>
+                                <th class="col-xs-2">
+                                    <span>Author(s):</span>
+                                </th>
+                                <td class="col-xs-10">
+                                    @foreach($book->authors as $i => $author)
+                                        @if($i == 0)
+                                            <span>{{ $author->full_name }}</span>
+                                        @else
+                                            <span>, {{ $author->full_name }}</span>
+                                        @endif
+                                    @endforeach
+                                </td>
+                            </tr>
 
-                                    <tr>
-                                        <th class="col-xs-2">ISBN-10:</th>
-                                        <td class="col-xs-10">{{ $book->isbn10 }}</td>
-                                    </tr>
+                            <tr>
+                                <th class="col-xs-2">ISBN-10:</th>
+                                <td class="col-xs-10">{{ $book->isbn10 }}</td>
+                            </tr>
 
-                                    <tr>
-                                        <th class="col-xs-2">ISBN-13:</th>
-                                        <td class="col-xs-10">{{ $book->isbn13 }}</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
+                            <tr>
+                                <th class="col-xs-2">ISBN-13:</th>
+                                <td class="col-xs-10">{{ $book->isbn13 }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
 
-                            </td>
-                        </tr>
-                    </table>
+                    <div class="col-md-2 col-sm-3 hidden-xs">
+                        @if(count($book->products) > 1)
+                            <span class="price">${{ $book->decimalLowestPrice() }}</span>
+                            <span class="text-muted"> ~ </span>
+                            <span class="price">${{ $book->decimalHighestPrice() }}</span>
+                        @else
+                            <span class="price">${{ $book->decimalLowestPrice() }}</span>
+                        @endif
+                    </div>
                 </div>
 
                 @empty
