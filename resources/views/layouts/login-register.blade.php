69875fd5741e86e6ca326f3769f780b8589803a8
kingdido999 <kingdido999@gmail.com>
2015-09-11 14:17:43 -0400

    add flash message section to login and signup page

--- resources/views/layouts/login-register.blade.php
+++ resources/views/layouts/login-register.blade.php
@@ -15,6 +15,9 @@
 </head>
 
 <body>
+    {{-- Session flash messages --}}
+    @include('includes.alerts')
+
     @yield('content')
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
