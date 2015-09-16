56e33529fda438b86a2c9bf4a21a2cbcb0c7c054
kingdido999 <kingdido999@gmail.com>
2015-09-16 15:11:41 -0400

    add product-delete modal for product show page

--- public/js/product/show.js
+++ public/js/product/show.js
@@ -1,12 +1,23 @@
 /**
  * Created by Desmond on 8/20/15.
- * slick: https://github.com/kenwheeler/slick/
  */
 
 $(document).ready(function(){
+    // slick: https://github.com/kenwheeler/slick/
     $('.image-slider').slick({
         autoplay: true,
         autoplaySpeed: 5000,
         dots: true
+    });
+
+    // product delete modal
+    $('#delete-product').on('show.bs.modal', function(event) {
+        var button = $(event.relatedTarget);
+        var productId = button.data('product-id');
+        var bookTitle = button.data('book-title');
+
+        var modal = $(this);
+        modal.find('input[name=id]').val(productId);
+        modal.find('.book-title').text(bookTitle);
     });
 });
