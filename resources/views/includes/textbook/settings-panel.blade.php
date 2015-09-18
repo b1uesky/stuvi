368f3e39bb69576e393967ca9c0ad532fdab9cfd
kingdido999 <kingdido999@gmail.com>
2015-09-18 14:43:57 -0400

    add panel-title

--- resources/views/includes/textbook/settings-panel.blade.php
+++ resources/views/includes/textbook/settings-panel.blade.php
@@ -1,5 +1,7 @@
 <div class="panel panel-default">
-    <div class="panel-heading">Personal Settings</div>
+    <div class="panel-heading">
+        <h3 class="panel-title">Personal Settings</h3>
+    </div>
 
     <div class="list-group">
         <a class="list-group-item {{ Request::path() == 'user/profile' ? 'active' : '' }}" href="{{ url('user/profile') }}">Profile Settings</a>
