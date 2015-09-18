f9fbdbf83b27087d4a5c867868621954edd9ff59
kingdido999 <kingdido999@gmail.com>
2015-09-18 16:06:12 -0400

    fix contact page display on mobile device

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,43 +4,48 @@
 
 @section('content')
 
-    <div class="container-fluid contact-container">
+    <div class="container">
         <div class="row">
-            <h2 class="contact-title">Contact Us</h2>
+            <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+                <div class="page-header">
+                    <h1>Contact us</h1>
+                </div>
 
-            <div class="contact-subtitle">
-                <p>Please feel free to ask a question or give us feedback.</p>
+                <div class="margin-bottom-15">
+                    <small class="text-muted">Please feel free to ask a question or give us feedback.</small>
+                </div>
+
+                <form action="/contact/store" method="post">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                    <div class="form-group">
+                        @if(Auth::check())
+                            <input type="text" class="form-control" name="name" value="{{ Auth::user()->fullName() }}"
+                                   placeholder="Name">
+                        @else
+                            <input type="text" class="form-control" name="name" placeholder="Name">
+                        @endif
+                    </div>
+
+                    <div class="form-group">
+                        @if(Auth::check())
+                            <input type="email" class="form-control" name="email"
+                                   value="{{ Auth::user()->primaryEmail->email_address }}" placeholder="Email">
+                        @else
+                            <input type="email" class="form-control" name="email" placeholder="Email">
+                        @endif
+                    </div>
+
+                    <div class="form-group">
+                        <textarea class="form-control" rows="8" name="message" placeholder="Message"></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-block btn-primary">Submit</button>
+                </form>
             </div>
-
-            <form action="/contact/store" method="post" class="form-horizontal">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-                <div class="form-group">
-                    @if(Auth::check())
-                        <input type="text" class="form-control" name="name" value="{{ Auth::user()->fullName() }}"
-                               placeholder="Name">
-                    @else
-                        <input type="text" class="form-control" name="name" placeholder="Name">
-                    @endif
-                </div>
-
-                <div class="form-group">
-                    @if(Auth::check())
-                        <input type="email" class="form-control" name="email"
-                               value="{{ Auth::user()->primaryEmail->email_address }}" placeholder="Email">
-                    @else
-                        <input type="email" class="form-control" name="email" placeholder="Email">
-                    @endif
-                </div>
-
-                <div class="form-group">
-                    <textarea class="form-control" rows="8" name="message" placeholder="Message"></textarea>
-                </div>
-
-                <button type="submit" class="btn btn-lg btn-block btn-primary">Submit</button>
-            </form>
-
         </div>
+
+        <br>
     </div>
 
 @endsection
