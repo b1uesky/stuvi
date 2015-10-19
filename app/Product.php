2b5111c4c36484bfdfbb80344ae9f235cb7c8ffc
kingdido999 <kingdido999@gmail.com>
2015-10-19 09:15:19 -0400

    use guarded instead of fillable

--- app/Product.php
+++ app/Product.php
@@ -5,16 +5,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = [
-        'book_id',
-        'price',
-        'book_id',
-        'seller_id',
-        'sold',
-        'verified',
-        'deleted_at',
-        'sell_to'
-    ];
+//    protected $fillable = [
+//        'book_id',
+//        'price',
+//        'book_id',
+//        'seller_id',
+//        'sold',
+//        'verified',
+//        'deleted_at',
+//        'sell_to'
+//    ];
+    protected $guarded = [];
 
     public function book()
     {
@@ -161,7 +162,6 @@ class Product extends Model
      * Validation rules.
      *
      * @param $images
-     * @param int $sell_to
      *
      * @return array
      */
