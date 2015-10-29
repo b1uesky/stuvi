55261fffbc4d922afafbaa837213ba4f8757e4ab
kingdido999 <kingdido999@gmail.com>
2015-10-29 13:36:41 -0400

    update about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,7 +10,7 @@
         </div>
 
         <p>
-            We are a team of Computer Science students at Boston University. We created Stuvi (Student Village) as a student solution service,
+            We are a team of students at Boston University. We created Stuvi (Student Village) as a student solution service,
             ranging from textbook deliveries, to managing academic life. To kick-off our launch, we're offering a new method to buy and sell
             your textbooks. Simply search for your book, describe its condition, and start making money.
         </p>
