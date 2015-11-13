2919f7f44b20f821ebacefe6bffc44e02f6eb7e4
kingdido999 <kingdido999@gmail.com>
2015-11-13 08:17:49 -0500

    modify confirm page

--- resources/views/textbook/confirm.blade.php
+++ resources/views/textbook/confirm.blade.php
@@ -12,47 +12,77 @@
             </ol>
         </div>
 
-        <br>
+        <div class="page-header">
+            <h1>Confirm your book</h1>
+        </div>
 
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-xs-12">
+                @include('includes.textbook.book-details')
 
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">
-                            Confirm the book
-                        </h3>
-                    </div>
+                <hr>
 
-                    <div class="panel-body">
-                        @include('includes.textbook.book-details-confirm')
-                    </div>
+                <div>
+                    @if(count($book->availableProducts()) > 0)
+                        <a href="{{ url('textbook/buy/' . $book->id) }}" class="btn btn-primary">Buy this book</a>
+                    @else
+                        <a href="#" class="btn btn-info disabled">Temporarily out of stock</a>
+                    @endif
 
-                    <div class="panel-footer">
-                        <div class="row">
-                            <div class="col-xs-6">
-                                @if(count($book->availableProducts()) > 0)
-                                    <a href="{{ url('textbook/buy/' . $book->id) }}" class="btn btn-primary btn-block">
-                                        Buy
-                                    </a>
-                                @else
-                                    <a href="#" class="btn btn-warning btn-block disabled">Temporarily out of stock</a>
-                                @endif
-                            </div>
-                            <div class="col-xs-6">
-                                @if(Auth::guest())
-                                    <a href="#" class="btn btn-default btn-block disabled">
-                                        Login or signup to sell this book
-                                    </a>
-                                @else
-                                    <a href="{{ url('textbook/sell/product/' . $book->id . '/create') }}" class="btn btn-default btn-block">Sell</a>
-                                @endif
-                            </div>
-                        </div>
-                    </div>
+                    <a href="{{ url('textbook/sell/product/' . $book->id . '/create') }}" class="btn btn-default">Sell this book</a>
                 </div>
 
+                <br>
+
+                @if(Auth::guest())
+                    <p class="text-info">
+                        <span class="glyphicon glyphicon-log-in"></span>
+                        To sell a book, you need to login first.
+                    </p>
+                @endif
             </div>
+
         </div>
+
+        {{--<div class="row">--}}
+            {{--<div class="col-md-8 col-md-offset-2">--}}
+
+                {{--<div class="panel panel-default">--}}
+                    {{--<div class="panel-heading">--}}
+                        {{--<h3 class="panel-title">--}}
+                            {{--Confirm the book--}}
+                        {{--</h3>--}}
+                    {{--</div>--}}
+
+                    {{--<div class="panel-body">--}}
+                        {{--@include('includes.textbook.book-details-confirm')--}}
+                    {{--</div>--}}
+
+                    {{--<div class="panel-footer">--}}
+                        {{--<div class="row">--}}
+                            {{--<div class="col-xs-6">--}}
+                                {{--@if(count($book->availableProducts()) > 0)--}}
+                                    {{--<a href="{{ url('textbook/buy/' . $book->id) }}" class="btn btn-primary btn-block">--}}
+                                        {{--Buy--}}
+                                    {{--</a>--}}
+                                {{--@else--}}
+                                    {{--<a href="#" class="btn btn-warning btn-block disabled">Temporarily out of stock</a>--}}
+                                {{--@endif--}}
+                            {{--</div>--}}
+                            {{--<div class="col-xs-6">--}}
+                                {{--@if(Auth::guest())--}}
+                                    {{--<a href="#" class="btn btn-default btn-block disabled">--}}
+                                        {{--Login or sign up to sell this book--}}
+                                    {{--</a>--}}
+                                {{--@else--}}
+                                    {{--<a href="{{ url('textbook/sell/product/' . $book->id . '/create') }}" class="btn btn-default btn-block">Sell</a>--}}
+                                {{--@endif--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+
+            {{--</div>--}}
+        {{--</div>--}}
     </div>
 @endsection
