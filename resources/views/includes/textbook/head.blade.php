2496e4da3589062329d90f378f838672de821cc6
kingdido999 <kingdido999@gmail.com>
2015-11-13 15:25:24 -0500

    add ubuntu font

--- resources/views/includes/textbook/head.blade.php
+++ resources/views/includes/textbook/head.blade.php
@@ -11,7 +11,7 @@
     <link rel="icon" type="image/png" href="https://s3.amazonaws.com/stuvi-logo/favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="https://s3.amazonaws.com/stuvi-logo/favicon-16x16.png" sizes="16x16" />
 
-    {{--<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600'>--}}
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Ubuntu:400,700,500'>
     <link rel="stylesheet" href="{{ asset('build/css/textbook.css') }}">
 
     @yield('css')
