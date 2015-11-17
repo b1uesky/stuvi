273f28a148cb34d7dca295379a578b526b4d2402
kingdido999 <kingdido999@gmail.com>
2015-11-17 08:14:06 -0500

    fix controller name

--- app/Http/Controllers/Textbook/BookReminderController.php
+++ app/Http/Controllers/Textbook/BookReminderController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class BookReminder extends Controller
+class BookReminderController extends Controller
 {
     /**
      * Display a listing of the resource.
