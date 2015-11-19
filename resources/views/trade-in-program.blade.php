cc32c917368389ed8ecf47db23d6338764f295b3
kingdido999 <kingdido999@gmail.com>
2015-11-19 15:59:46 -0500

    finished trade-in-program page

--- resources/views/trade-in-program.blade.php
+++ resources/views/trade-in-program.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-    <div class="trade-in-sections">
+    <div id="trade-in-sections">
         <section id="trade-in-header">
             <div class="jumbotron">
                 <div class="container">
@@ -23,13 +23,15 @@
             <div class="jumbotron">
                 <div class="container">
                     <div class="row">
-                        <div class="col-sm-6">
-
+                        <div class="col-sm-6 col-sm-push-6">
+                            <img class="center-block" src="https://s3.amazonaws.com/stuvi-graphics/checking-task-list-by-hand.png">
                         </div>
 
-                        <div class="col-sm-6">
-                            <h2>Post your book</h2>
-                            <p>Upload your textbook and make sure to check "I would like to join the Stuvi Book Trade-in Program".</p>
+                        <div class="col-sm-6 col-sm-pull-6">
+                            <h2>1. Post Your Book</h2>
+                            <p>Simply upload your textbook and check<br>
+                                <strong class="text-checkbox"><span class="glyphicon glyphicon-check"></span> I would like to join the Stuvi Book Trade-in Program</strong>.</p>
+                            <small class="text-muted">* You can join the program on your textbook page as well.</small>
                         </div>
                     </div>
                 </div>
@@ -41,13 +43,13 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-sm-6">
-                            <h2>Wating for approval</h2>
-                            <p>Our team will review your book, offer a trade-in price and send you an email with details once we approved your book.</p>
-                            <small>* This process may take at most 2 days.</small>
+                            <img class="center-block" src="https://s3.amazonaws.com/stuvi-graphics/people-in-meeting.png">
                         </div>
 
                         <div class="col-sm-6">
-
+                            <h2>2. Wating For Approval</h2>
+                            <p>Our team will review your book, offer a trade-in price and send you an email with details once we approved your book.</p>
+                            <small class="text-muted">* This process may take at most 2 days.</small>
                         </div>
                     </div>
                 </div>
@@ -58,13 +60,14 @@
             <div class="jumbotron">
                 <div class="container">
                     <div class="row">
-                        <div class="col-sm-6">
-
+                        <div class="col-sm-6 col-sm-push-6">
+                            <img class="center-block" src="https://s3.amazonaws.com/stuvi-graphics/money-bg-exchange.png">
                         </div>
 
-                        <div class="col-sm-6">
-                            <h2>Schedule and Get Paid</h2>
-                            <p>If you decide to take the deal, our courier will come to pickup your book and pay you at the time you scheduled.</p>
+                        <div class="col-sm-6 col-sm-pull-6">
+                            <h2>3. Schedule and Get Paid</h2>
+                            <p>After you accept the offer and schedule a pickup, our courier will come to pickup your book.</p>
+                            <small class="text-muted">* Get paid by cash or PayPal.</small>
                         </div>
                     </div>
                 </div>
@@ -76,7 +79,7 @@
         <div class="jumbotron">
             <div class="container">
                 <div class="row">
-                    <div class="text-center">
+                    <div class="text-center margin-30">
                         <a href="{{ url('/') }}" class="btn btn-lg btn-warning">Sell Your Textbook Now</a>
                     </div>
                 </div>
