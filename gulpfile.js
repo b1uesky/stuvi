1ba9bc36264590fddf7d124a7510ed51094b4c63
kingdido999 <kingdido999@gmail.com>
2015-11-24 13:08:12 -0500

    fix gulp watch infinite loops bug; use browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -12,40 +12,21 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function (mix) {
-    // fonts
-    //mix.copy('resources/assets/libs/bootstrap/fonts', 'public/build/fonts');
-
-    // core js
-    //mix.scripts([
-    //    '../libs/html5shiv/dist/html5shiv.min.js',
-    //    '../libs/respond/dest/respond.min.js',
-    //    '../libs/jquery/dist/jquery.min.js',
-    //    '../libs/bootstrap/dist/js/bootstrap.min.js',
-    //    '../libs/sortable/js/sortable.min.js',
-    //    '../libs/zoom.js/js/zoom.js',
-    //    'alert.js',
-    //], 'public/build/js/core.js');
-
-    // compile scss to css
-    mix.sass('textbook.scss', './public/build/css/textbook.css');
-    //mix.sass('admin.scss', './public/build/css/admin.css');
-    //mix.sass('express.scss', './public/build/css/express.css');
 
     // textbook css
-    mix.styles([
-        '../libs/jquery-ui/themes/smoothness/jquery-ui.min.css',
-        '../libs/bootstrap-select/dist/css/bootstrap-select.min.css',
-        '../libs/sortable/css/sortable-theme-minimal.css',
-        '../libs/zoom.js/css/zoom.css',
-        '../libs/dropzone/dist/min/dropzone.min.css',
-        '../libs/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css',
-        '../../../public/build/css/textbook.css'
-    ], 'public/build/css/textbook.css');
+    mix.sass('textbook.scss')
+        .styles([
+            '../libs/jquery-ui/themes/smoothness/jquery-ui.min.css',
+            '../libs/bootstrap-select/dist/css/bootstrap-select.min.css',
+            '../libs/sortable/css/sortable-theme-minimal.css',
+            '../libs/zoom.js/css/zoom.css',
+            '../libs/dropzone/dist/min/dropzone.min.css',
+            '../libs/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css'
+        ], 'public/build/css/textbook.css');
 
     // textbook js
-    mix.scriptsIn('resources/assets/js', 'public/build/js/textbook.js');
-
-    mix.scripts([
+    mix.scriptsIn('resources/assets/js')
+        .scripts([
         '../libs/html5shiv/dist/html5shiv.min.js',
         '../libs/respond/dest/respond.min.js',
         '../libs/jquery/dist/jquery.min.js',
@@ -59,8 +40,28 @@ elixir(function (mix) {
         '../libs/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
         '../libs/dropzone/dist/min/dropzone.min.js',
         '../libs-paid/formvalidation-dist-v0.6.3/dist/js/formValidation.min.js',
-        '../libs-paid/formvalidation-dist-v0.6.3/dist/js/framework/bootstrap.min.js',
-        '../../../public/build/js/textbook.js'
+        '../libs-paid/formvalidation-dist-v0.6.3/dist/js/framework/bootstrap.min.js'
     ], 'public/build/js/textbook.js');
+
+    //mix.sass('admin.scss', './public/build/css/admin.css');
+    //mix.sass('express.scss', './public/build/css/express.css');
+
+    // core js
+    //mix.scripts([
+    //    '../libs/html5shiv/dist/html5shiv.min.js',
+    //    '../libs/respond/dest/respond.min.js',
+    //    '../libs/jquery/dist/jquery.min.js',
+    //    '../libs/bootstrap/dist/js/bootstrap.min.js',
+    //    '../libs/sortable/js/sortable.min.js',
+    //    '../libs/zoom.js/js/zoom.js',
+    //    'alert.js',
+    //], 'public/build/js/core.js');
+
+    // fonts
+    //mix.copy('resources/assets/libs/bootstrap/fonts', 'public/build/fonts');
+
+    mix.browserSync({
+        proxy: 'stuvi.app' // use your own proxy here
+    });
 
 });
