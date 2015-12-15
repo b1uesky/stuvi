37dfcb38549015ef366e4949a999fcf299c8936c
kingdido999 <kingdido999@gmail.com>
2015-12-15 09:16:58 -0500

    clean up DonationController

--- app/Http/Controllers/Textbook/DonationController.php
+++ app/Http/Controllers/Textbook/DonationController.php
@@ -9,8 +9,6 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Input;
-use Illuminate\Support\Facades\Validator;
 
 class DonationController extends Controller
 {
@@ -55,50 +53,5 @@ class DonationController extends Controller
     {
         return view('donation.confirmation')
             ->with('donation', $donation);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
