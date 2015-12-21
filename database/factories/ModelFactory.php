5b0a530a2845bb5d9f3c8c78bab7ce8fbdc8e73d
kingdido999 <kingdido999@gmail.com>
2015-12-21 12:40:59 -0500

    add ProductTest (incomplete)

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,3 +17,35 @@ $factory->define(App\Email::class, function (Faker\Generator $faker) {
         'email_address' => $faker->firstName.'@bu.edu'
     ];
 });
+
+$factory->define(App\Product::class, function (Faker\Generator $faker) {
+    return [
+        'book_id'           => \App\Book::orderByRaw('RAND()')->first()->id,
+        'seller_id'         => \App\User::orderByRaw('RAND()')->first()->id,
+        'price'             => $faker->numberBetween(1, 1000),
+        'available_at'      => $faker->date(),
+        'payout_method'     => $faker->randomElement(['cash', 'paypal']),
+        'accept_trade_in'   => $faker->boolean(),
+        'verified'          => true
+    ];
+});
+
+$factory->define(App\ProductCondition::class, function (Faker\Generator $faker) {
+    return [
+        'product_id'            => null,
+        'general_condition'     => $faker->numberBetween(0, 2),
+        'highlights_and_notes'  => $faker->numberBetween(0, 2),
+        'damaged_pages'         => $faker->numberBetween(0, 2),
+        'broken_binding'        => $faker->boolean(),
+        'description'           => $faker->paragraph()
+    ];
+});
+
+$factory->define(App\ProductImage::class, function (Faker\Generator $faker) {
+    return [
+        'product_id'    => null,
+        'small_image'   => $faker->imageUrl(),
+        'medium_image'  => $faker->imageUrl(),
+        'large_image'   => $faker->imageUrl()
+    ];
+});
---
+++ tests/unit/ProductTest.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class ProductTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function test_saving_product()
+    {
+//        $product = factory(\App\Product::class)->create();
+//        $product_condition = factory(\App\ProductCondition::class)->create([
+//            'product_id'    => $product->id
+//        ]);
+//        $product_image = factory(\App\ProductImage::class)->create([
+//            'product_id'    => $product->id
+//        ]);
+
+//        $this->tester->seeInDatabase('products', $product->toArray());
+//        $this->tester->seeRecord('product_conditions', $product_condition->toArray());
+//        $this->tester->seeRecord('product_images', $product_image->toArray());
+    }
+}
