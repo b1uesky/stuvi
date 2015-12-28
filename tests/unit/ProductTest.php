59228c96fb0fbb01848b5f0e3537967a30f6a735
kingdido999 <kingdido999@gmail.com>
2015-12-28 11:46:23 -0500

    finish test_saving_product

--- tests/unit/ProductTest.php
+++ tests/unit/ProductTest.php
@@ -19,16 +19,20 @@ class ProductTest extends \Codeception\TestCase\Test
     // tests
     public function test_saving_product()
     {
-//        $product = factory(\App\Product::class)->create();
-//        $product_condition = factory(\App\ProductCondition::class)->create([
-//            'product_id'    => $product->id
-//        ]);
-//        $product_image = factory(\App\ProductImage::class)->create([
-//            'product_id'    => $product->id
-//        ]);
+        $product = factory(\App\Product::class)->create();
+        $product_condition = factory(\App\ProductCondition::class)->create([
+            'product_id'    => $product->id
+        ]);
+        $product_image = factory(\App\ProductImage::class)->create([
+            'product_id'    => $product->id
+        ]);
 
-//        $this->tester->seeInDatabase('products', $product->toArray());
-//        $this->tester->seeRecord('product_conditions', $product_condition->toArray());
-//        $this->tester->seeRecord('product_images', $product_image->toArray());
+        // price is stored as integer in db
+        $expected_product = $product->toArray();
+        $expected_product['price'] = \App\Helpers\Price::convertDecimalToInteger($product->price);
+
+        $this->tester->seeRecord('products', $expected_product);
+        $this->tester->seeRecord('product_conditions', $product_condition->toArray());
+        $this->tester->seeRecord('product_images', $product_image->toArray());
     }
 }
